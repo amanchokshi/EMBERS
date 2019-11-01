cb1bc1d916217a43e53021ac03c0f2c4e494eed3
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-01 16:26:18 +1100

    Major restructure. Complete docstrings. The only hardcoded value is the MWA site location

--- sat_ephemeris.py
+++ sat_ephemeris.py
@@ -1,11 +1,8 @@
-import json
 import numpy as np
 import skyfield as sf
 from astropy.time import Time
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from astroplan.plots import plot_sky
 from skyfield.api import Topos, load
+
 
 # Skyfield Timescale
 ts = load.timescale(builtin=True)
@@ -15,7 +12,18 @@ MWA = Topos(latitude=-26.703319, longitude=116.670815, elevation_m=337.83)
 
 
 def load_tle(tle_path):
-    '''Loads a tle file & returns list of sat objects and corresponding epochs''' 
+    '''Loads a tle.
+    
+    Opens TLE file and uses skyfield to load each pair of TLE lines
+    into an EarthSatellite object. Determines epoch for each.
+
+    Agrs:
+        the_path: Path to tle file
+
+    Returns:
+        sats: List of skyfield earth satellite objects
+        epochs: Time at which each set of TLE lines is most accurate
+    ''' 
     
     # open a Two Line Element (TLE) file
     with open(tle_path, 'r') as f:
@@ -37,8 +45,16 @@ def load_tle(tle_path):
 
 
 def epoch_ranges(epochs):
-    '''Creates a time array with intervals corresponding to 
-    epochs of best accuracy from the TLE file
+    '''Creates a time array
+    
+    Time array with intervals corresponding to epochs of 
+    best accuracy from the TLE pair of lines in TLE file.
+     
+     Args:
+        epochs: List of epochs derived from TLE file
+
+     Returns:
+        epoch_range: List of times, between which TLE is most accurate
     '''
     
     # Midpoints between epochs of successive TLEs
@@ -50,8 +66,18 @@ def epoch_ranges(epochs):
 
 
 def epoch_time_array(index_epoch, t_step):
-    '''Create a skyfield time object [time vector] at which
+    '''Create a time array.
+    
+    Skyfield time object [time vector] at which
     the sat position will be determined.
+
+    Args:
+        index_epoch: Index of epoch range to be converted to time array
+        t_step: Cadence of time array
+
+    Returns:
+        t_arr: Skyfield time object
+        index_epoch: Same as above
     '''
 
     # find time between epochs/ midpoints in seconds, using Astropy Time
@@ -71,11 +97,24 @@ def epoch_time_array(index_epoch, t_step):
     return (t_arr, index_epoch)
 
 
-
 def sat_pass(sats, t_arr, index_epoch):
-    '''Calculates alt/az of sat at every position instant of time array (t_arr).
-    Finds all the times that the sat is above the horizon, and returns the index
-    t_arr at which the sat rose and set.'''
+    '''Finds satellite passes.
+    
+    Calculates alt/az of sat at every position instant of time 
+    array (t_arr). Finds all the times that the sat is above
+    the horizon, and returns the index t_arr at which the sat
+    rose and set.
+    
+    Args:
+        sats: list of Skyfield EarthSatellite objects
+        t_arr: Skyfield time object
+        index_epoch: Position in epoch_range
+    
+    Returns:
+        passes: 2D array with pairs of indicies corresponding to rise/set of sat
+        alt: Array of altitudes of sat at t_arr times
+        az: Array of azimuths of sat at t_arr times
+    '''
 
     # Define Satellite to be the first one in sats list
     # Find possition of sat at each timestep of time array
@@ -107,7 +146,22 @@ def sat_pass(sats, t_arr, index_epoch):
 
 
 def ephem_data(t_arr, pass_index, alt, az):
-    #'''Writes rise, set times and alt, az arrays to json file.'''
+    '''Satellite Ephemeris Data.
+    
+    creates rise time, set times and alt, az arrays.
+    
+    Args:
+        t_arr: Skyfield time array object
+        pass_index: One pair of sat indicies from passed 2D array
+        alt: Array of altitudes of sat at t_arr times
+        az: Array of azimuths of sat at t_arr times
+
+    Returns:
+        t_rise: Rise time of sat in gps seconds (Astropy)
+        t_set: Set time of sat in gps seconds (Astropy)
+        sat_alt: Altitude of satellite while it is above the horizon. Array.
+        sat_az: Azimuth of satellite while it is above the horizon. Array.
+    '''
 
     i, j = pass_index
     t_rise = Time(t_arr[i].tt, format='jd').gps
@@ -121,63 +175,54 @@ def ephem_data(t_arr, pass_index, alt, az):
     
     return (t_rise, t_set, sat_alt, sat_az)
 
-#def plot_sat(pass_indices):
-#    '''Plots a satellite pass on a polar alt/az map'''
-#    i, j = pass_indices
-#
-#    # Set up the polar plot.
-#    plt.style.use('seaborn')
-#    ax = plt.subplot(111, polar=True)
-#    ax.set_ylim(90, 0)
-#    ax.set_rgrids([0,30,60,90], angle=22)
-#    #ax.set_xticklabels(['N', '', 'E', '', 'S', '', 'W', ''])
-#    ax.set_theta_zero_location('N')
-#    ax.set_theta_direction(-1)
-#
-#    # Draw line and labels.
-#    θ = az.radians
-#    r = alt.degrees
-#    ax.plot(θ[i:j+1], r[i:j+1], '-', linewidth=2, alpha=0.6, c='#1fab89')
-#    
-#    plt.tight_layout()
-#    #plt.show()
 
+if __name__ == '__main__':
+    
+    import os
+    import json
+    import argparse
 
-sat_ephem = {}
-sat_ephem['t_rise'] = []
-sat_ephem['t_set'] = []
-sat_ephem['sat_alt'] = []
-sat_ephem['sat_az'] = []
+    parser = argparse.ArgumentParser(description="""
+            Code which converts the TLE files downloaded with download_TLE.py
+            into satellite ephemeris data: rise time, set time, alt/az arrays
+            at a given time cadence. This is saved to a json file which will 
+            be used to plot the satellite passes.
+            """)
+            
+    parser.add_argument('--sat', metavar='\b', help='The Norad cat ID of satellite. Example: 21576')
+    parser.add_argument('--tle', metavar='\b', default='./outputs/TLE', help='Directory where TLE files are saved. Default=./outputs/TLE')
+    parser.add_argument('--cadence', metavar='\b', default=20, help='Rate at which sat alt/az is computed. Expensive! Default=20s')
+    
+    args = parser.parse_args()
+    sat_name = args.sat
+    tle_dir = args.tle
+    cadence = args.cadence
+    
+    tle_path = '{}/{}.txt'.format(tle_dir, sat_name)
+    
+    
+    os.makedirs(os.path.dirname('./outputs/ephem_json/'), exist_ok=True)
+    
+    sat_ephem = {}
+    sat_ephem['t_rise'] = []
+    sat_ephem['t_set'] = []
+    sat_ephem['sat_alt'] = []
+    sat_ephem['sat_az'] = []
+    
+    sats, epochs = load_tle(tle_path)
+    epoch_range = epoch_ranges(epochs)
+    for i in range(len(epoch_range) - 1):   
+        t_arr, index_epoch = epoch_time_array(i, cadence)
+        passes, alt, az = sat_pass(sats, t_arr, index_epoch) 
+        for pass_index in passes:
+            t_rise, t_set, sat_alt, sat_az = ephem_data(t_arr, pass_index, alt, az)
+    
+            sat_ephem['t_rise'].append(t_rise)
+            sat_ephem['t_set'].append(t_set)
+            sat_ephem['sat_alt'].append(sat_alt)
+            sat_ephem['sat_az'].append(sat_az)
+    
+    with open('./outputs/ephem_json/{}.json'.format(sat_name), 'w') as outfile:
+        json.dump(sat_ephem, outfile)    
 
-sats, epochs = load_tle('TLE/21576.txt')
-epoch_range = epoch_ranges(epochs)
-for i in range(len(epoch_range) - 1):   
-    t_arr, index_epoch = epoch_time_array(2, 20)
-    passes, alt, az = sat_pass(sats, t_arr, index_epoch) 
-    for pass_index in passes:
-        t_rise, t_set, sat_alt, sat_az = ephem_data(t_arr, pass_index, alt, az)
-
-        sat_ephem['t_rise'].append(t_rise)
-        sat_ephem['t_set'].append(t_set)
-        sat_ephem['sat_alt'].append(sat_alt)
-        sat_ephem['sat_az'].append(sat_az)
-
-with open('21576.json', 'w') as outfile:
-    json.dump(sat_ephem, outfile)    
-
-
-#print(len(epoch_range))
-#for i in range(len(epoch_range)-1):
-#
-#    t_arr, index_epoch = epoch_time_array(i, 20)
-#    passes, alt, az = sat_pass(t_arr, index_epoch)
-#
-#
-#    for i in range(len(passes)):
-#        if len(passes) == 0:
-#            print('No Satellite Passes')
-#        else:
-#            plot_sat(passes[i])
-#    
-#plt.show()
 
