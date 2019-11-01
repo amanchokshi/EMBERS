35c4e0d1541eafcf7f06ef48d64b5488e26a4d6d
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-01 21:02:27 +1100

    Added argparse

--- plot_ephemeris.py
+++ plot_ephemeris.py
@@ -1,24 +1,32 @@
 import json
+import argparse
 import matplotlib
 matplotlib.use
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+parser = argparse.ArgumentParser(description=""" 
+        Plot Satellite passes from json ephemeris files.
+        """)
+parser.add_argument('--sat', metavar='\b', help='Norad cat ID of satellite. Ex:23545')
+
+args = parser.parse_args()
+sat_num = args.sat
 
 def sat_plot(alt, az):
     # Set up the polar plot.
     plt.style.use('seaborn')
     ax = plt.subplot(111, polar=True)
     ax.set_ylim(90, 0)
-    ax.set_rgrids([0,30,60,90], angle=22)
+    ax.set_rgrids([0,10,20,30,40,50,60,70,80,90], angle=22)
     ax.set_theta_zero_location('N')
     ax.set_theta_direction(-1)
-    ax.plot(az, alt, '-', linewidth=1, alpha=0.2, c='#1fab89')
+    ax.plot(az, alt, '-', linewidth=1, alpha=0.3, c='#1fab89')
     plt.tight_layout()
     plt.show()        
 
 
-with open('./outputs/ephem_json/23545.json', 'r') as ephem:
+with open('./outputs/ephem_json/{}.json'.format(sat_num), 'r') as ephem:
     sat_ephem = json.load(ephem)
     altitude = []
     azimuth = []
