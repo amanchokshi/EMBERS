e5c48cf1f585f5b09e230ab6407e96996ae12fc3
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-02 17:44:50 +1100

    Seven parellel jobs

--- parallel_sat_ephem.sh
+++ parallel_sat_ephem.sh
@@ -4,7 +4,7 @@ while read p; do
   echo "python sat_ephemeris.py --sat=$p" >> parallel_ephem.txt
 done <sat_list.txt
 
-# GNU Parallel. 8 Jobs at a time
-parallel --jobs 8 < parallel_ephem.txt 
+# 8 Jobs in Parallel
+parallel --jobs 7 < parallel_ephem.txt 
 
 rm parallel_ephem.txt
