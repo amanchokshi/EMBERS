0aaf48df13c3af99cf66f45927ad4a9bbbc7a1e8
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-04 10:52:08 +1100

    Use sat_ids.py instead of sat_list.txt

--- code/sat-ephemeris/parallel_sat_ephem.sh
+++ code/sat-ephemeris/parallel_sat_ephem.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+python sat_ids >> sat_list.txt
+
 while read p; do
   echo "python sat_ephemeris.py --sat=$p" >> parallel_ephem.txt
 done <sat_list.txt
@@ -8,3 +10,4 @@ done <sat_list.txt
 parallel --jobs 7 < parallel_ephem.txt
 
 rm parallel_ephem.txt
+rm sat_list.txt
--- code/sat-ephemeris/plot_all_ephem.sh
+++ code/sat-ephemeris/plot_all_ephem.sh
@@ -1,1 +1,3 @@
- while read p; do python plot_ephemeris.py --sat=$p; done <sat_list.txt
+python sat_ids.py >> sat_list.txt
+while read p; do python plot_ephemeris.py --sat=$p; done <sat_list.txt
+rm sat_list.txt
