9d25bb29e77853c5bf684e109c994530520a9843
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-11-04 12:46:05 +1100

    Fixed small typo

--- code/sat-ephemeris/parallel_sat_ephem.sh
+++ code/sat-ephemeris/parallel_sat_ephem.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-python sat_ids >> sat_list.txt
+python sat_ids.py >> sat_list.txt
 
 while read p; do
   echo "python sat_ephemeris.py --sat=$p" >> parallel_ephem.txt
 done <sat_list.txt
 
 # 8 Jobs in Parallel
-parallel --jobs 7 < parallel_ephem.txt
+parallel --jobs 14 < parallel_ephem.txt
 
 rm parallel_ephem.txt
 rm sat_list.txt
