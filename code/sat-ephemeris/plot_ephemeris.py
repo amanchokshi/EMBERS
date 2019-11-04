1e080ffd2e78c9ad1a4a4b7069161ce1c97e3482
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-04 11:09:54 +1100

    Changed json dir to argparse input

--- code/sat-ephemeris/plot_ephemeris.py
+++ code/sat-ephemeris/plot_ephemeris.py
@@ -10,16 +10,18 @@ parser = argparse.ArgumentParser(description="""
 
 parser.add_argument('--sat', metavar='\b', help='Norad cat ID of satellite. Ex:23545')
 parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/ephem_plots/', help='Path to output directory. Default=./../../outputs/ephem_plots/')
+parser.add_argument('--json_dir', metavar='\b', default='./../../outputs/ephem_json/', help='Path to ephem_json directory. Default=./../../outputs/ephem_json/')
 
 args = parser.parse_args()
 sat_id = args.sat
 out_dir = args.out_dir
+json_dir = args.json_dir
 
 # Create output directory
 os.makedirs(os.path.dirname(out_dir), exist_ok=True) 
 
 
-with open('./outputs/ephem_json/{}.json'.format(sat_id), 'r') as ephem:
+with open('{}/{}.json'.format(json_dir,sat_id), 'r') as ephem:
     sat_ephem = json.load(ephem)
     altitude = sat_ephem['sat_alt']
     azimuth = sat_ephem['sat_az']
@@ -27,7 +29,7 @@ with open('./outputs/ephem_json/{}.json'.format(sat_id), 'r') as ephem:
     
     plt = sat_plot(sat_id, altitude, azimuth, num_passes)
     plt.savefig('{}/{}'.format(out_dir,sat_id))
-    #plt.show()
+    # plt.show()
 
 
 
