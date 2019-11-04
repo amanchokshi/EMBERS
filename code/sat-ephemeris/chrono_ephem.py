ceb59a5c3356ab2d274455a5313d6c44fc7a3916
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-04 23:30:17 +1100

    Finds all json in dir, and reads them to list

--- code/sat-ephemeris/chrono_ephem.py
+++ code/sat-ephemeris/chrono_ephem.py
@@ -7,16 +7,19 @@ t_set = []
 alt = []
 az = []
 
+for file in os.listdir('./../../outputs/ephem_json'):
+    if file.endswith('.json'):
+        f_path = os.path.join('./../../outputs/ephem_json', file)
 
-
-with open('./../../outputs/ephem_json/21576.json') as ephem:
-    sat_ephem = json.load(ephem)
-    az.extend(sat_ephem['sat_az'])
-    alt.extend(sat_ephem['sat_alt'])
-    t_set.extend(sat_ephem['t_set'])
-    t_rise.extend(sat_ephem['t_rise'])
-    sat_id.extend([sat_ephem['sat_id'][0] for i in range(len(t_rise))])
+        with open(f_path) as ephem:
+            sat_ephem = json.load(ephem)
+            az.extend(sat_ephem['sat_az'])
+            alt.extend(sat_ephem['sat_alt'])
+            t_set.extend(sat_ephem['t_set'])
+            t_rise.extend(sat_ephem['t_rise'])
+            sat_id.extend([sat_ephem['sat_id'][0] for i in range(len(t_rise))])
     
-print(sat_id)    
-print(t_set)
+print(len(sat_id))    
+print(len(t_set))
+print(len(az))
 
