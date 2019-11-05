bb55278246b1800bfe44be599aa8c935e43ada2f
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-05 14:30:31 +1100

    Now accepts terminal arguments

--- code/beam-pointings/download_pointings.sh
+++ code/beam-pointings/download_pointings.sh
@@ -2,8 +2,8 @@
 
 mkdir ./../../outputs/beam-pointings
 
-for i in {1..48}
+for i in {1..$1}
 do
-    wget "http://ws.mwatelescope.org/metadata/find?mintime=1252195218&maxtime=1255219218&extended=1&page=$i" -O ./../../outputs/beam-pointings/pointings_$i.json
-    sleep 77
+    wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam-pointings/pointings_$i.json
+    sleep 66
 done 
