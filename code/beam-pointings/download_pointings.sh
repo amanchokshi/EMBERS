638f35c6a8c0523d48a2c32e4b5b8b0912ff9070
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-05 15:00:49 +1100

    Two digit numbers, and check if dir exists

--- code/beam-pointings/download_pointings.sh
+++ code/beam-pointings/download_pointings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir ./../../outputs/beam-pointings
+mkdir -p ./../../outputs/beam-pointings
 
-for i in {1..$1}
+for i in {01..$1}
 do
     wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam-pointings/pointings_$i.json
     sleep 66
