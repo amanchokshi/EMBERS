0e0029fdde6f1a81b88a5fccee66514dde7b611b
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-05 11:46:13 +1100

    It works now. Creates an ultimate_ephem_list.json

--- code/sat-ephemeris/chrono_ephem.py
+++ code/sat-ephemeris/chrono_ephem.py
@@ -1,5 +1,7 @@
 import os
 import json
+import numpy as np
+from sat_ephemeris import sat_plot
 
 sat_id = []
 t_rise = []
@@ -18,8 +20,20 @@ for file in os.listdir('./../../outputs/ephem_json'):
             t_set.extend(sat_ephem['t_set'])
             t_rise.extend(sat_ephem['t_rise'])
             sat_id.extend([sat_ephem['sat_id'][0] for i in range(len(sat_ephem['t_rise']))])
-    
-print(len(sat_id))    
-print(len(t_set))
-print(len(az))
+
+
+# sort all the lists based on t_rise
+t_rise, t_set, alt, az, sat_id = zip(*sorted(zip(t_rise, t_set, alt, az, sat_id)))
+
+chrono_ephem = {}
+chrono_ephem['sat_id'] = list(sat_id)
+chrono_ephem['t_rise'] = list(t_rise)
+chrono_ephem['t_set'] = list(t_set)
+chrono_ephem['sat_alt'] = list(alt)
+chrono_ephem['sat_az'] = list(az)
+
+
+with open('./../../outputs/ephem_json/ultimate_ephem_list.json', 'w') as outfile:
+    json.dump(chrono_ephem, outfile)
+
 
