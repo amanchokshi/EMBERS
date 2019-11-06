ce0b36d4b1440b4ae4e7b8e54caeb4fddb87a1da
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-06 17:36:58 +1100

    Fixed None poinings problem

--- code/beam-pointings/pointing_list.py
+++ code/beam-pointings/pointing_list.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description="""
         Reads data from downloaded pointing matadata
@@ -33,8 +34,32 @@ for file in os.listdir(meta_dir):
 
 start, stop, pointings = zip(*sorted(zip(start, stop, pointings)))
 
-print(pointings)       
+start_gps = list(start)
+stop_gps = list(stop)
+pointings = list(pointings)
 
+# Every morning, tests are run on the recievers.
+# There are always 20 test observations of ~72 seconds
+# These tests are assigned gridpointing == None
+# The last of these is actually a zenith obs. 
+# I manually set the 20th obs from None to 0
+# because the time following this test, until the next obs is 
+# usually a Looonnnnng time, and the telesocope remains at zenith
+
+# if list of 20 Nones, 20th reset to 0!
+
+for i in range(len(pointings)):
+    if (i+19) < len(pointings):
+        if (pointings[i] == None and pointings[i+19] == None):
+            pointings[i+19] = 0
+
+obs_length = list(np.diff(np.asarray(start_gps)))
+pointings = pointings[:-1]
+end_gps = start_gps[1:]
+start_gps = start_gps[:-1]
+
+for i in range(len(pointings)):
+    print(start_gps[i], end_gps[i], obs_length[i], pointings[i])
 
 #pointing_list = {}
 #pointing_list['start_gps'] = []
