803c316a92ed9b5bdea78313a8061f5867ae4e75
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-08 16:28:41 +1100

    Cleaned up and added comments

--- code/decode-rf-data/read_data.py
+++ code/decode-rf-data/read_data.py
@@ -17,29 +17,26 @@ def read_data(filename):
         for line in lines:
             time, data = line.split('$Sp'.encode())
             times.append(time.decode())
-            #data_lines.append(data[:-2].decode('latin-1'))
+            
+            # List converts bytes to list of bytes
+            # The last two charachters are excluded - Newline char
             data_lines.append(list(data[:-2]))
+        
+        
         #print(list(data_lines[0]))
         #power_lines = []
+        
+        
         print(len(data_lines[0]))
         water = np.array(data_lines)
         print(water.shape)
-        #water.shape = (len(times), len(data_lines))
+        
+        
         fig = plt.figure(figsize = (7,7))
         ax = fig.add_subplot(111)
         ax.imshow(water)
         ax.set_aspect('auto')
         plt.show()
-
-        #for d in data_lines:
-        #    powers = []
-        #    for char in d[:-2]:
-        #        powers.append(ord(char))
-        #    power_lines.append(powers)    
-
-
-        #times = np.asarray(times).astype(np.float)
-        #print(power_lines)    
 
 
 read_data('./../../sample-data/rf0XX_2019-10-10-15:00.txt')
