71f5a10d74deb9470f4aaae1ef35813fb9cb8693
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-09 15:09:26 +1100

    Saves plot to file in output dir, doesnt use x-window backend. Makes output dir if it doesnt exist

--- code/decode-rf-data/plot_waterfall.py
+++ code/decode-rf-data/plot_waterfall.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description="""
 
 parser.add_argument('--rf_dir', metavar='\b', default='./../../sample-data/', help='Path to rf data directory. Default=./../../sample-data/')
 parser.add_argument('--rf_name', metavar='\b', default='rf0XX_2019-10-10-15:00', help='Name of rf data file. Default=rf0XX_2019-10-10-15:00')
-parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/decode-rf-data', help='Output dir. Default=./../../outputs/decode-rf_dir')
+parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/decode-rf-data/', help='Output dir. Default=./../../outputs/decode-rf_dir/')
 
 args = parser.parse_args()
 rf_dir = args.rf_dir
@@ -17,12 +17,11 @@ rf_name = args.rf_name
 out_dir = args.out_dir
 
 
+power, times = rf.read_data('{}/{}.txt'.format(rf_dir,rf_name))
+plt = rf.plot_waterfall(power, times, rf_name)
+
 # Make output dir if it doesn't exist
 os.makedirs(os.path.dirname(out_dir), exist_ok=True)
 
-
-power, times = rf.read_data('{}{}.txt'.format(rf_dir,rf_name))
-plt = rf.plot_waterfall(power, times, rf_name)
 plt.savefig('{}/{}.png'.format(out_dir,rf_name))
-
 
