9eb717594eb5899dc934eae97285d5bde637d490
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-11-18 13:33:40 +1100

    Average sampling rate of rf explorers

--- code/align_data/align_data.py
+++ code/align_data/align_data.py
@@ -7,7 +7,7 @@ tile_list = rf.tile_names()
 
 
 for i in rf.tile_names():
-    _, times = rf.read_data(f'/media/achokshi/satellites/tiles_data/{i}/2019-11-15/{i}_2019-11-15-02:30.txt')
+    _, times = rf.read_data(f'/media/achokshi/satellites/tiles_data/{i}/2019-11-15/{i}_2019-11-15-03:30.txt')
     print(f'{i}: {len(times)/(30*60):.2f} Hz')
 
 
