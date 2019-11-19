ad9e0a841a7705a7a18726d346f49d32510e8c8b
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-11-19 22:01:08 +1100

    Fixed up plot, comments, etc

--- code/align_data/align_data.py
+++ code/align_data/align_data.py
@@ -5,6 +5,7 @@ import numpy as np
 from rf_data import read_data
 
 import matplotlib
+# Enable x-window display
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
@@ -88,6 +89,8 @@ ref_t, ref_p, tile_t, tile_p = time_align('./../../data/rf0XX_2019-10-10-02:30.t
 
 print(f'△ T = {(ref_t[-1] - ref_t[0]) - (tile_t[-1] - tile_t[0])} seconds')
 
-plt.plot(ref_p[:, 4])
 
+plt.style.use('seaborn')
+plt.plot(ref_p[:, 4], color='#7da87b')
+plt.tight_layout()
 plt.show()
