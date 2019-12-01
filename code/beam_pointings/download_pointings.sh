0d8ada9edc02132ba261a5475a5afd59a0940f82
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-02 10:26:57 +1100

    Fixed output folder

--- code/beam_pointings/download_pointings.sh
+++ code/beam_pointings/download_pointings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p ./../../outputs/beam-pointings
+mkdir -p ./../../outputs/beam_pointings
 
 for i in {01..$1}
 do
-    wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam-pointings/pointings_$i.json
+    wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam_pointings/pointings_$i.json
     sleep 66
 done 
