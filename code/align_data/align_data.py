ecf617e5a560f24fc2ab6b6bf9ff4b0b98bd04d6
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-03 19:52:38 +1100

    Savgol along time axis of 2D power array. Inter along same axis. Save time, ref_p, tile_p to hdf5. Deleted plotting stuff.

--- code/align_data/align_data.py
+++ code/align_data/align_data.py
@@ -1,4 +1,5 @@
 import math
+import h5py
 import numpy as np
 
 #TODO needed to add code dir to PYTHONPATH. Is this the best way?
@@ -8,6 +9,8 @@ import matplotlib
 # Enable x-window
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+#plt.rcParams.update({'font.size': 32})
+from matplotlib.pyplot import figure
 
 from scipy import interpolate
 from scipy.signal import savgol_filter
@@ -90,19 +93,30 @@ def time_align(ref, tile):
     
 ref_t, ref_p, tile_t, tile_p = time_align('./../../data/rf0XX_2019-10-10-02:30.txt', './../../data/S10XX_2019-10-10-02:30.txt') 
 
-#for channel in range(112):
-for channel in list(range(56,61)):
-    #savgol_sat = savgol_filter(ref_p, 123, 2, axis=0)
-    
-    savgol_ref = savgol_filter(ref_p[::, channel], 123, 1)
-    savgol_tile = savgol_filter(tile_p[::, channel], 123, 1)
+
+def savgol_interp(ref_t, ref_p, tile_t, tile_p):
+    """Smooth and interpolate the power array
+
+    Smooth the power arrays with a savgol filter
+    and them interpolate to a given frequency.
+    This makes the dimentions of the power arrays
+    from thr reference antenna and the tile equal,
+    allowing a one to one comparison at 
+    corresponding times.
+
+    """
+
+
+
+    savgol_ref = savgol_filter(ref_p, 151, 1, axis=0)
+    savgol_tile = savgol_filter(tile_p, 151, 1, axis=0)
     
     
     # Data is recorded at a range of frequencies, depending on the RF Explorer version
     # The Old models recoreded at 6-7Hz, while the new ones record at 8.5-9.2Hz.
     # To get around this, we interpolate the data to a desired freqeuncy.
     
-    interp_freq = 10 #Hz
+    interp_freq = 6 #Hz
     
     # Using the start and stop times, we create an array of times at which to 
     # evaluate our interpolated data
@@ -113,40 +127,25 @@ for channel in list(range(56,61)):
     time_seconds = stop_time - start_time
     
     # Array of times at which to evaluate the interpolated data
-    time_steps = np.linspace(start_time, stop_time, (time_seconds * interp_freq))
+    time_array = np.linspace(start_time, stop_time, (time_seconds * interp_freq))
     
     # Interp1d output functions
-    f = interpolate.interp1d(ref_t, savgol_ref, kind='cubic')
-    g = interpolate.interp1d(tile_t, savgol_tile, kind='cubic')
+    f = interpolate.interp1d(ref_t, savgol_ref, axis=0, kind='cubic')
+    g = interpolate.interp1d(tile_t, savgol_tile, axis=0, kind='cubic')
     
     # New power array, evaluated at the desired frequency
-    ref_p_n = f(time_steps)
-    tile_p_n = g(time_steps)
+    ref_p_aligned = f(time_array)
+    tile_p_aligned = g(time_array)
+
+    with h5py.File('test.hdf5', 'w') as f:
+        f.create_dataset('ref_p_aligned', data=ref_p_aligned)
+        f.create_dataset('tile_p_aligned', data=tile_p_aligned)
+        f.create_dataset('time_array', data=time_array)
+
+
+
     
-    
-    # Plots 
-    plt.style.use('seaborn')
-    
-    plt.scatter(ref_t, ref_p[::, channel], color='#abcb89',marker='.', alpha=0.7, label='ref tile')
-    plt.plot(ref_t,savgol_ref, color='#25a55f', alpha=0.9, label='ref savgol')
-    
-    
-    plt.scatter(tile_t, tile_p[::, channel], color='#ea7362',marker='.', alpha=0.7, label='aut tile')
-    plt.plot(tile_t,savgol_tile, color='#a64942', alpha=0.9, label='aut savgol')
-    
-    #plt.imshow(savgol_sat)
-    #plt.axis('auto')
-    
-    leg = plt.legend(loc="upper left", frameon=True)
-    leg.get_frame().set_facecolor('white')
-    for l in leg.legendHandles:
-        l.set_alpha(1)
-    
-    plt.xlabel('Time')
-    plt.ylabel('Power')
-    plt.xlim(1570646750, 1570647600)
-    plt.title(f'Frequency Channel {channel}')
-    plt.tight_layout()
-    plt.savefig(f'channel-{channel}.png')
-    #plt.show()
-    plt.close()
+savgol_interp(ref_t, ref_p, tile_t, tile_p)
+
+
+
