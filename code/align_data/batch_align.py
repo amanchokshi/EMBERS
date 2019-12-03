a0d4eabf6b33042776590807b748cba2d7ae598c
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-03 21:34:04 +1100

    Import align_data

--- code/align_data/batch_align.py
+++ code/align_data/batch_align.py
@@ -1,8 +1,8 @@
 import h5py
+import align_data as ali
 
-
-    with h5py.File('test.hdf5', 'w') as f:
-        f.create_dataset('ref_p_aligned', data=ref_p_aligned)
-        f.create_dataset('tile_p_aligned', data=tile_p_aligned)
-        f.create_dataset('time_array', data=time_array)
+#with h5py.File('test.hdf5', 'w') as f:
+#    f.create_dataset('ref_p_aligned', data=ref_p_aligned)
+#    f.create_dataset('tile_p_aligned', data=tile_p_aligned)
+#    f.create_dataset('time_array', data=time_array)
 
