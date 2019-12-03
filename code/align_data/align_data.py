2538127bbe785c8075d70f3e92c5d6f107b7af0e
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-03 14:42:56 +1100

    Make plots of each channel

--- code/align_data/align_data.py
+++ code/align_data/align_data.py
@@ -90,57 +90,63 @@ def time_align(ref, tile):
     
 ref_t, ref_p, tile_t, tile_p = time_align('./../../data/rf0XX_2019-10-10-02:30.txt', './../../data/S10XX_2019-10-10-02:30.txt') 
 
-#savgol_sat = savgol_filter(ref_p, 123, 2, axis=0)
-
-savgol_ref = savgol_filter(ref_p[::, 4], 123, 1)
-savgol_tile = savgol_filter(tile_p[::, 4], 123, 1)
-
-
-# Data is recorded at a range of frequencies, depending on the RF Explorer version
-# The Old models recoreded at 6-7Hz, while the new ones record at 8.5-9.2Hz.
-# To get around this, we interpolate the data to a desired freqeuncy.
-
-interp_freq = 10 #Hz
-
-# Using the start and stop times, we create an array of times at which to 
-# evaluate our interpolated data
-start_time = math.ceil(max(ref_t[0], tile_t[0]))
-stop_time = math.floor(min(ref_t[-1], tile_t[-1]))
-
-# Total length of observation in seconds
-time_seconds = stop_time - start_time
-
-# Array of times at which to evaluate the interpolated data
-time_steps = np.linspace(start_time, stop_time, (time_seconds * interp_freq))
-
-# Interp1d output functions
-f = interpolate.interp1d(ref_t, savgol_ref, kind='cubic')
-g = interpolate.interp1d(tile_t, savgol_tile, kind='cubic')
-
-# New power array, evaluated at the desired frequency
-ref_p_n = f(time_steps)
-tile_p_n = g(time_steps)
-
-
-# Plots 
-plt.style.use('seaborn')
-
-plt.scatter(ref_t, ref_p[::, 4], color='#abcb89',marker='.', alpha=0.7, label='ref tile')
-plt.plot(ref_t,savgol_ref, color='#25a55f', alpha=0.9, label='ref savgol')
-
-
-plt.scatter(tile_t, tile_p[::, 4], color='#ea7362',marker='.', alpha=0.7, label='aut tile')
-plt.plot(tile_t,savgol_tile, color='#a64942', alpha=0.9, label='aut savgol')
-
-#plt.imshow(savgol_sat)
-#plt.axis('auto')
-
-leg = plt.legend(loc="upper left", frameon=True)
-leg.get_frame().set_facecolor('white')
-for l in leg.legendHandles:
-    l.set_alpha(1)
-
-plt.xlabel('Time')
-plt.ylabel('Power')
-plt.tight_layout()
-plt.show()
+#for channel in range(112):
+for channel in list(range(56,61)):
+    #savgol_sat = savgol_filter(ref_p, 123, 2, axis=0)
+    
+    savgol_ref = savgol_filter(ref_p[::, channel], 123, 1)
+    savgol_tile = savgol_filter(tile_p[::, channel], 123, 1)
+    
+    
+    # Data is recorded at a range of frequencies, depending on the RF Explorer version
+    # The Old models recoreded at 6-7Hz, while the new ones record at 8.5-9.2Hz.
+    # To get around this, we interpolate the data to a desired freqeuncy.
+    
+    interp_freq = 10 #Hz
+    
+    # Using the start and stop times, we create an array of times at which to 
+    # evaluate our interpolated data
+    start_time = math.ceil(max(ref_t[0], tile_t[0]))
+    stop_time = math.floor(min(ref_t[-1], tile_t[-1]))
+    
+    # Total length of observation in seconds
+    time_seconds = stop_time - start_time
+    
+    # Array of times at which to evaluate the interpolated data
+    time_steps = np.linspace(start_time, stop_time, (time_seconds * interp_freq))
+    
+    # Interp1d output functions
+    f = interpolate.interp1d(ref_t, savgol_ref, kind='cubic')
+    g = interpolate.interp1d(tile_t, savgol_tile, kind='cubic')
+    
+    # New power array, evaluated at the desired frequency
+    ref_p_n = f(time_steps)
+    tile_p_n = g(time_steps)
+    
+    
+    # Plots 
+    plt.style.use('seaborn')
+    
+    plt.scatter(ref_t, ref_p[::, channel], color='#abcb89',marker='.', alpha=0.7, label='ref tile')
+    plt.plot(ref_t,savgol_ref, color='#25a55f', alpha=0.9, label='ref savgol')
+    
+    
+    plt.scatter(tile_t, tile_p[::, channel], color='#ea7362',marker='.', alpha=0.7, label='aut tile')
+    plt.plot(tile_t,savgol_tile, color='#a64942', alpha=0.9, label='aut savgol')
+    
+    #plt.imshow(savgol_sat)
+    #plt.axis('auto')
+    
+    leg = plt.legend(loc="upper left", frameon=True)
+    leg.get_frame().set_facecolor('white')
+    for l in leg.legendHandles:
+        l.set_alpha(1)
+    
+    plt.xlabel('Time')
+    plt.ylabel('Power')
+    plt.xlim(1570646750, 1570647600)
+    plt.title(f'Frequency Channel {channel}')
+    plt.tight_layout()
+    plt.savefig(f'channel-{channel}.png')
+    #plt.show()
+    plt.close()
