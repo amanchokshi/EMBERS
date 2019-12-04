b879fbb619a96f4d4a21dbd0658e5dded7817b5f
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-04 21:16:33 +1100

    Changed output dir name

--- code/sat_ephemeris/download_tle.py
+++ code/sat_ephemeris/download_tle.py
@@ -16,7 +16,7 @@ if os.environ.get('ST_USER') != None:
     parser = argparse.ArgumentParser(description="""Downloads TLE files for the Beam Experiment from Space-tracks.org.""")
     parser.add_argument('--start_date', metavar='\b', help='Start date in format YYYY-MM-DD')
     parser.add_argument('--stop_date', metavar='\b', help='Stop date in format YYYY-MM-DD')
-    parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/sat-ephemeris/TLE/', help='Path to output directory. Default=./../../outputs/sat-ephemeris/TLE/')
+    parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/sat_ephemeris/TLE/', help='Path to output directory. Default=./../../outputs/sat_ephemeris/TLE/')
 
     args = parser.parse_args()
 
