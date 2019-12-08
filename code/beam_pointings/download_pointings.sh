29ec0a96843bf32c2393b581bbcbd4b04b02bcfb
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-09 10:30:47 +1100

    changed sleep from 66s to 44s. I am not that patient

--- code/beam_pointings/download_pointings.sh
+++ code/beam_pointings/download_pointings.sh
@@ -5,5 +5,5 @@ mkdir -p ./../../outputs/beam_pointings
 for i in {001..$1}
 do
     wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam_pointings/pointings_$i.json
-    sleep 66
+    sleep 44
 done 
