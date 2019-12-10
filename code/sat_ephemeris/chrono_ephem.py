64e6a39f9b449faa7dda348f10375c5dce6c5799
Aman Chokshi <amanchokshi.iisc@gmail.com>
2019-12-11 03:54:32 +1100

    Fixed az issue, and save out to chrono_json dir

--- code/sat_ephemeris/chrono_ephem.py
+++ code/sat_ephemeris/chrono_ephem.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description="""
         """)
 
 parser.add_argument('--json_dir', metavar='\b', default='./../../outputs/sat_ephemeris/ephem_json/', help='Directory where ephem json files live. Default=./../../outputs/sat_ephemeris/ephem_json/')
-parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/sat_ephemeris/ephem_chrono_json/', help='Output directory. Default=./../../outputs/sat_ephemeris/chrono_json/')
+parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/sat_ephemeris/chrono_json/', help='Output directory. Default=./../../outputs/sat_ephemeris/chrono_json/')
 parser.add_argument('--interp_type', metavar='\b', default='cubic', help='Type of interpolation. Ex: Cubic,Linear. Default=Cubic')
 parser.add_argument('--interp_freq', metavar='\b', default=2, help='Frequency at which to interpolate, in Hertz. Must be the same as used in align_data.py. Default=2')
 parser.add_argument('--start_date', metavar='\b', help='Date from which to determine sat ephemeris. Ex: 2019-10-10')
@@ -153,91 +153,30 @@ for file in os.listdir(json_dir):
                                 s_az,
                                 interp_type,
                                 interp_freq)
+                        print(tm_interp[0], st_alt[0], st_az[0])
                         
-                        print(tm_interp)
+                        #print(tm_interp)
                         time_array.append(tm_interp)    
                         sat_alt.append(st_alt)
-                        sat_alt.append(st_az)
+                        sat_az.append(st_az)
 
                     else:
                         pass
 
-                        s_ephem = {}
-                        s_ephem['sat_id'] = sat_id
-                        s_ephem['time_array'] = time_array
-                        s_ephem['sat_alt'] = sat_alt
-                        s_ephem['sat_az'] = sat_az
+                s_ephem = {}
+                s_ephem['sat_id'] = sat_id
+                s_ephem['time_array'] = time_array
+                s_ephem['sat_alt'] = sat_alt
+                s_ephem['sat_az'] = sat_az
 
-                        Path(out_dir).mkdir(parents=True, exist_ok=True)
-                        with open(f'{out_dir}/{obs_time[t]}.json', 'w') as outfile:
-                            json.dump(s_ephem, outfile, indent=4)
+                Path(out_dir).mkdir(parents=True, exist_ok=True)
+
+                if time_array != []:
+                    with open(f'{out_dir}/{obs_time[t]}.json', 'w') as outfile:
+                        json.dump(s_ephem, outfile, indent=4)
 
         break
            
-            
-#for file in os.listdir(json_dir):
-#    if file.endswith('.json'):
-#        f_path = os.path.join(json_dir, file)
-#        
-#        with open(f_path) as ephem:
-#            sat_ephem = json.load(ephem)
-#
-#            t_array = sat_ephem['time_array']
-#            s_alt   = sat_ephem['sat_alt']
-#            s_az    = sat_ephem['sat_az']
-#            
-#            #time_array = []
-#            #sat_alt = []
-#            #sat_az = []
-#            #sat_id = []
-#           
-#            
-#            for p in range(len(t_array)):       # p == pass
-#
-#                # Don't consider passes with less than 4 samples (~ 1 min)
-#                if len(t_array[p]) > 3:
-#                   
-#                    # Convert gps time from Skyfield to unix time
-#                    # Because rf explorers record data with unix timestamp
-#                    times_unix = Time(t_array[p], format='gps').unix
-#                    
-#                    # Create interpolation functions
-#                    alt_interp = interpolate.interp1d(times_unix, s_alt[p], kind=interp_type)
-#                    az_interp  = interpolate.interp1d(times_unix, s_az[p], kind=interp_type)
-#                    
-#                    # Determine a integer interval between which to interpolate
-#                    t_start = math.ceil(times_unix[0])
-#                    t_stop  = math.floor(times_unix[-1])
-#
-#                    # Times array, at which to determine alt/az of sat
-#                    time_interp = list(np.double(np.arange(t_start, t_stop, (1/interp_freq))))
-#                    #time_array.extend(time_interp)
-#
-#                    n_steps = len(time_interp)
-#                    #sat_num = list(np.repeat(int(sat_ephem['sat_id'][0]), n_steps))
-#                    sat_num = sat_ephem['sat_id'] * n_steps
-#                    #sat_id.extend(sat_num)
-#                    
-#                    #sat_alt.extend(list(alt_interp(time_interp)))
-#                    #sat_alt.extend(list(alt_interp(time_interp)))
-#                    sat_az = az_interp(time_interp)
-#                    sat_az = az_interp(time_interp)
-#
-#                    print(min(time_interp))
-#                    print(max(time_interp))
-#
-#                else:
-#                    pass
-#                break
-#        break              
-
-#mask = list(np.argwhere( (np.asarray(time_array)) > obs_unix[0]) & (np.asarray(time_array) < obs_unix[1]) )
-#print(list(compress(time_array, mask)))
-#print(len(time_array))
-
-
-## Save collated sat ephem data to one big json file
-#with open(f'{json_dir}/ultimate_ephem_list.json', 'w') as outfile:
-#    json.dump(chrono_ephem, outfile, indent=4)
+           
 
 
