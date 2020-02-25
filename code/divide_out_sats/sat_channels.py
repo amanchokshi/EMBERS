6a49b1da0d40d256aa478cf8386c444f10eb5490
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-02-25 15:44:41 +1100

    Outline of script to find sat channels

---
+++ code/divide_out_sats/sat_channels.py
@@ -0,0 +1,8 @@
+#TODO Import a dataset
+#TODO Find the median of the data. This will give us an approximate of the noise floor
+#TODO Loop through freq channels and find any data above the median - potential satellite
+#TODO Read chrono_ephem json file for that particular obs. Sort passes by lenght(time in sky)
+#TODO If the potential sat occupies more than 80% of the sat pass length, classify it as a sat!
+#TODO Or, come up with alternative thresholding scheme.
+#TODO Exclude that channel from next loop
+#TODO Find a way to include more channels - weather sats
