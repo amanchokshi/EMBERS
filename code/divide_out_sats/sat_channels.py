5f2fd1b409780fde5d19bf0284c2a19f53ba9fb0
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-02-26 15:35:03 +1100

    Read ref data file and compute noise floor

--- code/divide_out_sats/sat_channels.py
+++ code/divide_out_sats/sat_channels.py
@@ -7,9 +7,27 @@
 #TODO Or, come up with alternative thresholding scheme.
 #TODO Exclude that channel from next loop
 #TODO Find a way to include more channels - weather sats
-
-
-
 #TODO Use reference data for sat, with corresponding chrono json for ephem
 
+    
+import numpy as np
+import matplotlib.pyplot as plt
 
+import sys
+sys.path.append('../decode_rf_data')
+from rf_data import read_data, plot_waterfall
+
+
+ref_file = '../../../tiles_data/rf0XX/2019-10-07/rf0XX_2019-10-07-00:00.txt'
+
+power, times = read_data(ref_file)
+
+# To first order, let us consider the median to be the noise floor
+noise_f = np.median(power)
+print(noise_f)
+print(power)
+
+
+plot_waterfall(power, times, 'rf0XX')
+plt.show()
+    
