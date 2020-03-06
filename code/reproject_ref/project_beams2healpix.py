3265b85e9ce5e4093168d0c98088718e42bfab8b
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-06 19:38:54 +1100

    The FEE models are read and plotted successfuly. Need to understand the script and make it my own

--- code/reproject_ref/project_beams2healpix.py
+++ code/reproject_ref/project_beams2healpix.py
@@ -2,10 +2,10 @@
 # https://github.com/JLBLine/MWA_ORBCOMM
 
 
-from numpy import *
-import matplotlib.pyplot as plt
+#from numpy import *
+import numpy as np
 import healpy as hp
-from my_plotting_lib import add_colourbar
+import matplotlib.pyplot as plt
 from scipy import interpolate
 
 
@@ -17,44 +17,68 @@ def plot_healpix(data_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None
     '''Does some plotting what what'''
     if vmin == None:
         if cmap == None:
-            half_sky = hp.orthview(map=data_map,coord='E',half_sky=True,xsize=400,title=title,rot=(0,90,0),sub=sub,notext=True,return_projected_map=True)
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,
+                    title=title,rot=(0,90,0),
+                    sub=sub,notext=True,
+                    return_projected_map=True)
         else:
-            half_sky = hp.orthview(map=data_map,coord='E',half_sky=True,xsize=400,title=title,rot=(0,90,0),sub=sub,cmap=cmap,notext=True,return_projected_map=True)
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,
+                    title=title,rot=(0,90,0),
+                    sub=sub,cmap=cmap,notext=True,
+                    return_projected_map=True)
     else:
         if cmap == None:
-            half_sky = hp.orthview(map=data_map,coord='E',half_sky=True,xsize=400,title=title,rot=(0,90,0),sub=sub,min=vmin,max=vmax,notext=True,return_projected_map=True)
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,
+                    title=title,rot=(0,90,0),
+                    sub=sub,min=vmin,max=vmax,
+                    notext=True,return_projected_map=True)
         else:
-            half_sky = hp.orthview(map=data_map,coord='E',half_sky=True,xsize=400,title=title,rot=(0,90,0),sub=sub,min=vmin,max=vmax,cmap=cmap,notext=True,return_projected_map=True)
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,
+                    title=title,rot=(0,90,0),
+                    sub=sub,min=vmin,max=vmax,
+                    cmap=cmap,notext=True,
+                    return_projected_map=True)
+
     hp.graticule(dpar=10,coord='E',color='k',alpha=0.3,dmer=45)
+    
     #print where(half_sky == nan)
 
 
-    hp.projtext(0.0*(pi/180.0), 0.0, '0', coord='E')
-    hp.projtext(30.0*(pi/180.0), 0.0, '30', coord='E')
-    hp.projtext(60.0*(pi/180.0), 0.0, '60', coord='E')
+    hp.projtext(0.0*(np.pi/180.0), 0.0, '0', coord='E')
+    hp.projtext(30.0*(np.pi/180.0), 0.0, '30', coord='E')
+    hp.projtext(60.0*(np.pi/180.0), 0.0, '60', coord='E')
 
 
-    hp.projtext(90.0*(pi/180.0), 0.0, r'$0^\circ$', coord='E',color='k',verticalalignment='top')
-    hp.projtext(90.0*(pi/180.0), 90.0*(pi/180.0), r'$90^\circ$', coord='E',color='k',horizontalalignment='right')
-    hp.projtext(90.0*(pi/180.0), 180.0*(pi/180.0), r'$180^\circ$', coord='E',color='k')
-    hp.projtext(90.0*(pi/180.0), 270.0*(pi/180.0), r'$270^\circ$', coord='E',color='k')
+    hp.projtext(90.0*(np.pi/180.0), 0.0, r'$0^\circ$', coord='E',color='k',verticalalignment='top')
+    hp.projtext(90.0*(np.pi/180.0), 90.0*(np.pi/180.0), r'$90^\circ$', coord='E',color='k',horizontalalignment='right')
+    hp.projtext(90.0*(np.pi/180.0), 180.0*(np.pi/180.0), r'$180^\circ$', coord='E',color='k')
+    hp.projtext(90.0*(np.pi/180.0), 270.0*(np.pi/180.0), r'$270^\circ$', coord='E',color='k')
 
 
 from scipy.interpolate import RectSphereBivariateSpline,SmoothSphereBivariateSpline
 ##Had problems with having coords = 0 when interpolating, so make a small number
 ##and call it epsilon for some reason
+
 epsilon = 1.e-12
-#@profile
+
 def create_model(file_name=None,tag=None):
     '''Takes .ffe model, converts into healpix and smooths the response'''
 
     ##Make an empty array for healpix projection
-    beam_response = zeros(len_empty_healpix)*nan
+    beam_response = np.zeros(len_empty_healpix)*np.nan
 
     ##Load ffe model data in, which is stored in real and imaginary components
     ##of a phi/theta polaristaion representation of the beam
     ##apparently this is normal to beam engineers
-    data = loadtxt(file_name)
+    data = np.loadtxt(file_name)
     theta = data[:,0]
     phi = data[:,1]
     re_theta = data[:,2]
@@ -70,14 +94,14 @@ def create_model(file_name=None,tag=None):
     X_phi.imag = im_phi
 
     ##make a coord grid for the data in the .ffe model
-    theta_range = linspace(epsilon,90,91)
-    phi_range = linspace(epsilon,359.0,360)
-    theta_mesh,phi_mesh = meshgrid(theta_range,phi_range)
+    theta_range = np.linspace(epsilon,90,91)
+    phi_range = np.linspace(epsilon,359.0,360)
+    theta_mesh,phi_mesh = np.meshgrid(theta_range,phi_range)
 
     ##Convert reference model into power from the complex values
     ##make an inf values super small
-    power = 10*log10(abs(X_theta)**2 + abs(X_phi)**2)
-    power[where(power == -inf)] = -80
+    power = 10*np.log10(abs(X_theta)**2 + abs(X_phi)**2)
+    power[np.where(power == -np.inf)] = -80
 
     ##Had a problem with edge effects, leave off near horizon values
     power = power[:-91]
@@ -86,10 +110,10 @@ def create_model(file_name=None,tag=None):
     ##s is a paramater I had to play with to get by eye nice results
     power.shape = phi_mesh.shape
 
-    lut = RectSphereBivariateSpline(theta_range*(pi/180.0), phi_range*(pi/180.0), power.T,s=0.1)
+    lut = RectSphereBivariateSpline(theta_range*(np.pi/180.0), phi_range*(np.pi/180.0), power.T,s=0.1)
 
     ##Get the theta and phi of all healpixels
-    ip = arange(len_empty_healpix)
+    ip = np.arange(len_empty_healpix)
     theta_rad, phi_rad = hp.pix2ang(nside, ip)
 
     ##Use spline to map beam into healpixels
@@ -97,8 +121,13 @@ def create_model(file_name=None,tag=None):
 
     return beam_response,theta_mesh,phi_mesh,power,theta
 
-healpix_east,theta_mesh,phi_mesh,power_east,theta = create_model(file_name='MWA_single_dipole_finite_gnd_Eastern_FarField1.ffe',tag='Eastern')
-healpix_west,theta_mesh,phi_mesh,power_west,theta = create_model(file_name='MWA_single_dipole_finite_gnd_Western_FarField1.ffe',tag='Western')
+
+refXX = '../../data/FEE_Reference_Models/MWA_reference_tile_FarField_XX.ffe'
+refYY = '../../data/FEE_Reference_Models/MWA_reference_tile_FarField_YY.ffe'
+
+
+healpix_east,theta_mesh,phi_mesh,power_east,theta = create_model(file_name=refXX, tag='Eastern')
+healpix_west,theta_mesh,phi_mesh,power_west,theta = create_model(file_name=refYY, tag='Western')
 
 ##Plot the things to sanity check and save results
 fig = plt.figure(figsize=(10,10))
@@ -106,12 +135,12 @@ fig = plt.figure(figsize=(10,10))
 ax1 = fig.add_subplot(221,projection='polar')
 ax2 = fig.add_subplot(222,projection='polar')
 
-im1 = ax1.pcolormesh(phi_mesh*(pi/180.0),theta_mesh,power_east,label='Eastern',vmin=-40,vmax=-20)
+im1 = ax1.pcolormesh(phi_mesh*(np.pi/180.0),theta_mesh,power_east,label='Eastern',vmin=-40,vmax=-20)
 ax1.set_title('Eastern')
 
 ax1.grid(color='k',alpha=0.3)
 
-im2 = ax2.pcolormesh(phi_mesh*(pi/180.0),theta_mesh,power_west,label='Western',vmin=-40,vmax=-20)
+im2 = ax2.pcolormesh(phi_mesh*(np.pi/180.0),theta_mesh,power_west,label='Western',vmin=-40,vmax=-20)
 ax2.set_title('Western')
 
 ax2.grid(color='k',alpha=0.3)
@@ -119,6 +148,6 @@ ax2.grid(color='k',alpha=0.3)
 plot_healpix(data_map=healpix_east,sub=(2,2,3),title='Healpix Eastern',vmin=-40,vmax=-20)
 plot_healpix(data_map=healpix_west,sub=(2,2,4),title='Healpix Western',vmin=-40,vmax=-20)
 
-savez_compressed('ung_dipole_models.npz',eastern=healpix_east,western=healpix_west)
+np.savez_compressed('ung_dipole_models.npz',eastern=healpix_east,western=healpix_west)
 
 fig.savefig('reproject_dipole_models.png',bbox_inches='tight')
