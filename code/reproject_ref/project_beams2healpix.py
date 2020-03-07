5ae04b7e8cb7a89afb3ef362b54e62e8d2643ae5
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-07 22:22:21 +1100

    Plot seems to finally work. Changed output dir to ../../outputs/reproject_ref, using Pathlib

--- code/reproject_ref/project_beams2healpix.py
+++ code/reproject_ref/project_beams2healpix.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import healpy as hp
+from pathlib import Path
 import matplotlib.pyplot as plt
 from scipy import interpolate
 from scipy.interpolate import RectSphereBivariateSpline,SmoothSphereBivariateSpline
@@ -76,14 +77,6 @@ def create_model(file_name=None):
 def plot_healpix(beam_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None):
     '''Does some plotting what what'''
     if vmin == None:
-        #if cmap == None:
-        #    half_sky = hp.orthview(
-        #            map=beam_map,coord='E',
-        #            half_sky=True,xsize=400,
-        #            title=title,rot=(0,90,0),
-        #            sub=sub,notext=True,
-        #            return_projected_map=True)
-        #else:
         half_sky = hp.orthview(
                 map=beam_map,coord='E',
                 half_sky=True,xsize=400,
@@ -91,18 +84,10 @@ def plot_healpix(beam_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None
                 sub=sub,cmap=cmap,notext=True,
                 return_projected_map=True)
     else:
-        #if cmap == None:
-        #    half_sky = hp.orthview(
-        #            map=beam_map,coord='E',
-        #            half_sky=True,xsize=400,
-        #            title=title,rot=(0,90,0),
-        #            sub=sub,min=vmin,max=vmax,
-        #            notext=True,return_projected_map=True)
-        #else:
         half_sky = hp.orthview(
                 map=beam_map,coord='E',
-                half_sky=True,xsize=400,
-                title=title,rot=(0,90,0),
+                half_sky=True,xsize=800,
+                title=title, rot=(0,90,0),
                 sub=sub,min=vmin,max=vmax,
                 cmap=cmap,notext=True,
                 return_projected_map=True)
@@ -110,19 +95,19 @@ def plot_healpix(beam_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None
     hp.graticule(dpar=10,coord='E',color='k',alpha=0.3,dmer=45)
     
     
-    hp.projtext(0.0*(np.pi/180.0), 0.0, '0', coord='E')
+    hp.projtext(00.0*(np.pi/180.0), 0.0, '00', coord='E')
     hp.projtext(30.0*(np.pi/180.0), 0.0, '30', coord='E')
     hp.projtext(60.0*(np.pi/180.0), 0.0, '60', coord='E')
 
 
-    hp.projtext(90.0*(np.pi/180.0), 0.0, r'$0^\circ$', coord='E',color='k',verticalalignment='top')
+    hp.projtext(90.0*(np.pi/180.0), 00.0, r'$0^\circ$', coord='E',color='k',verticalalignment='top')
     hp.projtext(90.0*(np.pi/180.0), 90.0*(np.pi/180.0), r'$90^\circ$', coord='E',color='k',horizontalalignment='right')
     hp.projtext(90.0*(np.pi/180.0), 180.0*(np.pi/180.0), r'$180^\circ$', coord='E',color='k')
     hp.projtext(90.0*(np.pi/180.0), 270.0*(np.pi/180.0), r'$270^\circ$', coord='E',color='k')
 
 
 
-#Healpix constants
+# Healpix constants
 nside = 32
 len_empty_healpix = hp.nside2npix(nside)   #12288
 
@@ -135,6 +120,9 @@ epsilon = 1.e-12
 refXX = '../../data/FEE_Reference_Models/MWA_reference_tile_FarField_XX.ffe'
 refYY = '../../data/FEE_Reference_Models/MWA_reference_tile_FarField_YY.ffe'
 
+# Output directory
+out_dir = '../../outputs/reproject_ref'
+Path(out_dir).mkdir(parents=True, exist_ok=True)
 
 healpix_XX,theta_mesh,phi_mesh,power_XX,theta = create_model(file_name=refXX)
 healpix_YY,theta_mesh,phi_mesh,power_YY,theta = create_model(file_name=refYY)
@@ -143,22 +131,35 @@ healpix_YY,theta_mesh,phi_mesh,power_YY,theta = create_model(file_name=refYY)
 fig = plt.figure(figsize=(10,10))
 
 ax1 = fig.add_subplot(221,projection='polar')
+ax1.set_rgrids([10,30,50,70,90], angle=22)
 ax2 = fig.add_subplot(222,projection='polar')
+ax2.set_rgrids([10,30,50,70,90], angle=22)
 
-im1 = ax1.pcolormesh(phi_mesh*(np.pi/180.0),theta_mesh,power_XX,label='XX',vmin=-40,vmax=-20, cmap=cmap)
-ax1.set_title('XX')
+im1 = ax1.pcolormesh(phi_mesh*(np.pi/180.0),
+        theta_mesh,power_XX,label='XX',
+        vmin=-40,vmax=-20, cmap=cmap)
 
+ax1.set_title('Ref XX', y=1.1)
 ax1.grid(color='k',alpha=0.3)
 
-im2 = ax2.pcolormesh(phi_mesh*(np.pi/180.0),theta_mesh,power_YY,label='YY',vmin=-40,vmax=-20, cmap=cmap)
-ax2.set_title('YY')
 
+im2 = ax2.pcolormesh(phi_mesh*(np.pi/180.0),
+        theta_mesh,power_YY,label='YY',
+        vmin=-40,vmax=-20, cmap=cmap)
+
+ax2.set_title('Ref YY', y=1.1)
 ax2.grid(color='k',alpha=0.3)
 
-plot_healpix(beam_map=healpix_XX,sub=(2,2,3),title='Healpix XX',vmin=-40,vmax=-20, cmap=cmap)
-plot_healpix(beam_map=healpix_YY,sub=(2,2,4),title='Healpix YY',vmin=-40,vmax=-20, cmap=cmap)
+plot_healpix(beam_map=healpix_XX,sub=(2,2,3),title=None,vmin=-40,vmax=-20, cmap=cmap)
+plot_healpix(beam_map=healpix_YY,sub=(2,2,4),title=None,vmin=-40,vmax=-20, cmap=cmap)
 
-#np.savez_compressed('ref_dipole_models.npz',XX=healpix_XX, YY=healpix_YY)
+ax3 = fig.add_subplot(2,2,3)
+ax3.axis('off')
+ax3.set_title('Healpix XX',x=0.4, y=-0.33)
 
-#fig.savefig('reproject_dipole_models.png',bbox_inches='tight')
-plt.show()
+ax4 = fig.add_subplot(2,2,4)
+ax4.axis('off')
+ax4.set_title('Healpix YY',x=0.6, y=-0.33)
+
+np.savez_compressed(f'{out_dir}/ref_dipole_models.npz',XX=healpix_XX, YY=healpix_YY)
+fig.savefig(f'{out_dir}/reproject_dipole_models.png',bbox_inches='tight')
