3dbba7ec779e481d024bac4e6c093e74ccf72b66
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-10 14:34:46 +1100

    Better plot title

--- code/sat_channels/channels_plt.py
+++ code/sat_channels/channels_plt.py
@@ -120,7 +120,7 @@ def sat_plot(out_dir, ids, norad_id, alt, az, num_passes, date):
     ax.set_rgrids([0,30,60,90], angle=22)
     ax.set_theta_zero_location('N')
     ax.set_theta_direction(-1)
-    ax.set_title(f'Satellites in {date}: {num_passes} Passes', y=1.08)
+    ax.set_title(f'{num_passes} satellite passes in {date} [{norad_id}] window', y=1.05)
     ax.grid(color='grey', linewidth=1.6, alpha=0.6)
 
     colors = pl.cm.Spectral(np.linspace(0,1,len(alt)))
