27fb663766074cabf11d844e6f172a84b70a3299
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-12 20:01:12 +1100

    Created power ephem to extract channel power and corresponding sat ephemeris

--- code/null_test/pass_healpix.py
+++ code/null_test/pass_healpix.py
@@ -1,6 +1,3 @@
-# Lets try and plot one pass to healpix
-
-
 import math
 # Force matplotlib to not use X-Server backend
 #import matplotlib
@@ -36,6 +33,62 @@ cmap = spectral()
 import sat_ids
 
 from channels_plt import plt_waterfall_pass, plt_channel, plt_hist, sat_plot
+
+
+
+def power_ephem(ref_file, chrono_file, sat_id, sat_chan):
+
+    power, times = savgol_interp(ref_file, savgol_window, polyorder, interp_type, interp_freq )
+    
+    # To first order, let us consider the median to be the noise floor
+    noise_f = np.median(power)
+    noise_mad = mad(power, axis=None)
+    
+    # Scale the power to bring the median noise floor down to zero
+    power = power - noise_f
+    
+    with open(chrono_file) as chrono:
+        chrono_ephem = json.load(chrono)
+    
+        norad_list = [chrono_ephem[s]["sat_id"][0] for s in range(len(chrono_ephem))]
+        
+        try:
+            norad_index = norad_list.index(sat_id)
+        
+        except Exception:
+            print(f'{sat_id}: not in {chrono_file}')
+            return 0
+        
+        norad_ephem = chrono_ephem[norad_index]
+            
+        rise_ephem  = norad_ephem["time_array"][0] 
+        set_ephem   = norad_ephem["time_array"][-1]
+        
+        intvl = time_filter(rise_ephem, set_ephem, np.asarray(times))
+        
+        if intvl != None:
+            
+            w_start, w_stop = intvl
+        
+            # Slice [crop] the power/times arrays to the times of sat pass
+            channel_power = power[w_start:w_stop+1, sat_chan]
+            times_c = times[w_start:w_stop+1]
+
+    
+            times_sat = norad_ephem["time_array"]
+            
+            # Crop ephem of all sats to size of norad_id sat
+            intvl_ephem = time_filter(times_c[0], times_c[-1], np.asarray(times_sat))
+            
+            if intvl_ephem != None:
+                e_0, e_1 = intvl_ephem
+                    
+                alt = norad_ephem["sat_alt"][e_0:e_1+1]
+                az  = norad_ephem["sat_az"][e_0:e_1+1]
+
+    return [channel_power, alt, az]
+
+        
     
 
 if __name__=='__main__':
@@ -70,69 +123,17 @@ if __name__=='__main__':
     #Path(out_dir).mkdir(parents=True, exist_ok=True)
     #sys.stdout = open(f'{out_dir}/Satellite_Channels_{start_date}_{stop_date}.txt', 'a')
 
-    data_dir = '../../../tiles_data'
-    chrono_dir = '../../outputs/sat_ephemeris/chrono_json'
-
     ref_file = Path(f'{data_dir}/rf0XX/2019-10-04/rf0XX_2019-10-04-16:00.txt')
     chrono_file = Path(f'{chrono_dir}/2019-10-04-16:00.json')
 
     sat_id = 41180
-    ch_no = 52
-    
-    power, times = savgol_interp(ref_file, savgol_window, polyorder, interp_type, interp_freq )
-    
-    # To first order, let us consider the median to be the noise floor
-    noise_f = np.median(power)
-    noise_mad = mad(power, axis=None)
-    
-    # Scale the power to bring the median noise floor down to zero
-    power = power - noise_f
-    
-    with open(chrono_file) as chrono:
-        chrono_ephem = json.load(chrono)
-    
-        num_passes = len(chrono_ephem)
-        
-        norad_list = [chrono_ephem[s]["sat_id"][0] for s in range(num_passes)]
-        
-        if sat_id in norad_list:
-            norad_index = norad_list.index(sat_id)
-        
-            norad_ephem = chrono_ephem[norad_index]
-                
-            rise_ephem  = norad_ephem["time_array"][0] 
-            set_ephem   = norad_ephem["time_array"][-1]
-            
-            intvl = time_filter(rise_ephem, set_ephem, np.asarray(times))
-        
-            if intvl != None:
-                
-                w_start, w_stop = intvl
-                
-                # Slice [crop] the power/times arrays to the times of sat pass
-                power_c = power[w_start:w_stop+1, :]
-                times_c = times[w_start:w_stop+1]
+    sat_chan = 52
+   
+    sat_data = power_ephem(ref_file, chrono_file, sat_id, sat_chan)
 
-                channel_power = power_c[:, 52]
-    
-                #plt.scatter(times_c, channel_power)
-                #plt.savefig('test.png')
-
-    
-
-                        
-                times_sat = norad_ephem["time_array"]
-                
-                # Crop ephem of all sats to size of norad_id sat
-                intvl_ephem = time_filter(times_c[0], times_c[-1], np.asarray(times_sat))
-                
-                if intvl_ephem != None:
-                    e_0, e_1 = intvl_ephem
-                        
-                    alt = norad_ephem["sat_alt"][e_0:e_1+1]
-                    az  = norad_ephem["sat_az"][e_0:e_1+1]
-                    
-                    print(len(times_c), len(alt), len(az))
+    if sat_data != 0:
+        channel_power, alt, az = sat_data
+        print(len(channel_power), len(alt), len(az))
 
 
 
