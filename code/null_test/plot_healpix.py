fdc97de216211acf1aefa5e7e753a7d75515cad1
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-13 21:00:12 +1100

    Changed formating so its easier to read

--- code/null_test/plot_healpix.py
+++ code/null_test/plot_healpix.py
@@ -6,25 +6,30 @@ import healpy as hp
 
 def plot_healpix(data_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None):
     '''Yeesh do some healpix magic to plot the thing'''
+    
     if vmin == None:
         if cmap == None:
-            half_sky = hp.orthview(map=data_map,coord='E',
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
                     half_sky=True,xsize=400,
                     title=title,rot=(0,90,0),
                     sub=sub,notext=True, return_projected_map=True)
         else:
-            half_sky = hp.orthview(map=data_map,coord='E',
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
                     half_sky=True,xsize=400,
                     title=title,rot=(0,90,0), sub=sub,cmap=cmap,
                     notext=True,return_projected_map=True)
     else:
         if cmap == None:
-            half_sky = hp.orthview(map=data_map,coord='E'
+            half_sky = hp.orthview(
+                    map=data_map,coord='E'
                     ,half_sky=True,xsize=400,rot=(0,90,0),
                     title=title,sub=sub,min=vmin,max=vmax,
                     notext=True,return_projected_map=True)
         else:
-            half_sky = hp.orthview(map=data_map,coord='E',
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
                     half_sky=True,xsize=400,rot=(0,90,0),
                     title=title,sub=sub,min=vmin,max=vmax,
                     cmap=cmap,notext=True,return_projected_map=True)
@@ -40,6 +45,5 @@ def plot_healpix(data_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None
     hp.projtext(90.0*(np.pi/180.0), 90.0*(np.pi/180.0), r'$90^\circ$', coord='E',color='k',horizontalalignment='right')
     hp.projtext(90.0*(np.pi/180.0), 180.0*(np.pi/180.0), r'$180^\circ$', coord='E',color='k')
     hp.projtext(90.0*(np.pi/180.0), 270.0*(np.pi/180.0), r'$270^\circ$', coord='E',color='k')
-
     
 
