ee6d42cf2c1fb63633974ac9ca2d12f59635386a
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-14 20:47:39 +1100

    Beginning null_test code by reading npz files

---
+++ code/null_test/null_test.py
@@ -0,0 +1,40 @@
+if __name__=='__main__':
+    
+    import argparse
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from pathlib import Path
+    
+    import sys
+    sys.path.append('../decode_rf_data')
+    from colormap import spectral
+    
+    # Custom spectral colormap
+    cmap = spectral()
+    
+    parser = argparse.ArgumentParser(description="""
+        Plot healpix map of reference data
+        """)
+    
+    parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/null_test/',help='Output directory. Default=./../../outputs/null_test/')
+    
+    args = parser.parse_args()
+    
+    out_dir = Path(args.out_dir)
+    
+    for f in out_dir.glob('*.npz'):
+        f_name, _ = f.name.split('.')
+        ref, _, _ = f_name.split('_')
+        
+        # load data from map .npz file
+        map_data = np.load(f, allow_pickle=True)
+        ref_map = map_data['ref_map']
+        ref_counter = map_data['ref_counter']
+        
+        # compute the median for every pixel array
+        ref_map_med = [(np.median(i) if i != [] else np.nan ) for i in ref_map]
+        vmin = np.nanmin(ref_map_med)
+        vmax = np.nanmax(ref_map_med)
+
+
+
