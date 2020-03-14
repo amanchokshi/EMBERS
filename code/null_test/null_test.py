ef559de00c41c415a6e3ad75d199ac5ede99f6a5
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-15 02:20:00 +1100

    Use new ref_map_slices function. Need to think of a way to distinguish b/w +/- ve θ

--- code/null_test/null_test.py
+++ code/null_test/null_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import healpy as hp
 
+
 def hp_slices_horizon(nside=None):
     '''Healpix pix indices of NS, EW slices and above horizon'''
     # theta phi values of each pixel 
@@ -34,6 +35,29 @@ def hp_slices_horizon(nside=None):
     return [NS_indices, EW_indices, above_horizon_indices]
 
 
+def ref_map_slice(ref_tile):
+    '''slices healpix map along NS, EW'''
+    
+    # load data from map .npz file
+    map_data = np.load(f'{out_dir}/{ref_tile}_map_healpix.npz', allow_pickle=True)
+    ref_map = map_data['ref_map']
+    ref_counter = map_data['ref_counter']
+    
+    # compute the median for every pixel array
+    ref_map_med = np.asarray([(np.median(i) if i != [] else np.nan ) for i in ref_map])
+    ref_map_mad = np.asarray([mad(i) for i in ref_map])
+
+    NS_indices, EW_indices, _ = hp_slices_horizon(nside)
+
+    θ_NS, _ = hp.pix2ang(nside, NS_indices)
+    θ_EW, _ = hp.pix2ang(nside, EW_indices)
+
+    NS_data = [ref_map[NS_indices], ref_map_med[NS_indices], ref_map_mad[NS_indices], θ_NS]
+    EW_data = [ref_map[EW_indices], ref_map_med[EW_indices], ref_map_mad[EW_indices], θ_EW]
+
+    return [NS_data, EW_data]
+
+
 if __name__=='__main__':
     
     import argparse
@@ -41,6 +65,8 @@ if __name__=='__main__':
     import matplotlib.pyplot as plt
     from pathlib import Path
     from scipy.stats import median_absolute_deviation as mad
+
+    import matplotlib.pyplot as plt
     
     import sys
     sys.path.append('../decode_rf_data')
@@ -63,31 +89,13 @@ if __name__=='__main__':
     nside   = args.nside
     
     ref_tiles = ['rf0XX', 'rf0YY', 'rf1XX', 'rf1YY']
-
     ref_tile = 'rf0XX'
     
-    def ref_map_slice(ref_tile):
-        
-        # load data from map .npz file
-        map_data = np.load(f'{out_dir}/{ref_tile}_map_healpix.npz', allow_pickle=True)
-        ref_map = map_data['ref_map']
-        ref_counter = map_data['ref_counter']
-        
-        # compute the median for every pixel array
-        ref_map_med = np.asarray([(np.median(i) if i != [] else np.nan ) for i in ref_map])
-        ref_map_mad = np.asarray([mad(i) for i in ref_map])
-
-        NS_indices, EW_indices, _ = hp_slices_horizon(nside)
-
-        θ_NS, _ = hp.pix2ang(nside, NS_indices)
-        θ_EW, _ = hp.pix2ang(nside, EW_indices)
-
-        NS_data = [ref_map[NS_indices], ref_map_med[NS_indices], ref_map_mad[NS_indices], θ_NS]
-        EW_data = [ref_map[EW_indices], ref_map_med[EW_indices], ref_map_mad[EW_indices], θ_EW]
-
-        return [NS_data, EW_data]
-
     NS_data, EW_data = ref_map_slice(ref_tile)
+    
+    _, ref_map_med_NS, ref_map_mad_NS, θ_NS = NS_data
+    _, ref_map_med_EW, ref_map_mad_EW, θ_EW = EW_data
+
         
         #break
 
