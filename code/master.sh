e8e7eba9e7e3342f1eade50492fe773f4e560149
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-03-17 10:18:15 +1100

    Align all data

--- code/master.sh
+++ code/master.sh
@@ -8,4 +8,7 @@
 # Plot waterfalls for one month of data
 python batch_waterfall.py --data_dir=../../../tiles_data --start_date=2019-10-01 --stop_date=2019-11-01
 
+# align all data
+python batch_align.py --data_dir=./../../../tiles_data --start_date=2019-09-12 --stop_date=2020-03-16
+
 
