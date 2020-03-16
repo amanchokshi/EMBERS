2953bf9ef6bc4c1c08077e49f82700f1a1364e40
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-03-17 10:11:58 +1100

    Plotting waterfalls for one month of data

--- code/master.sh
+++ code/master.sh
@@ -4,3 +4,8 @@
 ## MUHAHAHAHA HA HA ...
 
 # Batch process all data :)
+
+# Plot waterfalls for one month of data
+python batch_waterfall.py --data_dir=../../../tiles_data --start_date=2019-10-01 --stop_date=2019-11-01
+
+
