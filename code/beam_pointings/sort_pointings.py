3a070b2999eac2360c7d8b524b29d7bb604dcb82
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-18 01:49:55 +1100

    Im so cumfused. Cant seem to condense this list of obs into consecutive times

--- code/beam_pointings/sort_pointings.py
+++ code/beam_pointings/sort_pointings.py
@@ -62,33 +62,79 @@ for f in files:
                     pointings.append(pointing)
 
 for i in range(len(start_gps)):
-    print(f'{start_gps[i]}: {stop_gps[i]}: {pointings[i]}: {obs_length[i]}')
+    print(f'{pointings[i]}: {start_gps[i]}: {stop_gps[i]}: {obs_length[i]}')
 
-# if the stop time of one obs, and the start time of the next obs are same, they are consevutive.
-# if they also have the pointing, let us combine them. To do this, create new start, stop list, 
-# with corresponding pointing lists
 
-# remove first element
-start_1 = start_gps[1:]
-p_1 = pointings[1:]
-
-# remove last element
-stop_0 = stop_gps[:-1]
-p_0 = pointings[:-1]
-
-#consecutive_obs = np.where(np.asarray(start_1) == np.asarray(stop_0))
-#consecutive_poi =  np.where(np.asarray(p_1) == np.asarray(p_0)) 
+#begin_gps = []
+#end_gps = []
+#obs_length = []
+#obs_pointing = []
 #
-#print(len(consecutive_obs[0]))
-#print(len(consecutive_poi[0]))
-
-good_idx = np.where((np.asarray(start_1) == np.asarray(stop_0)) & (np.asarray(p_1) == np.asarray(p_0)))[0]
-
-#for i in good_idx:
-#    print(i)
-
-
+#for j in range(len(start_gps)):
+#    if j != (len(start_gps)-1):
+#        if ((stop_gps[j] != start_gps[j+1]) and (pointings[j] == pointings[j+1])):
+#            begin_gps.append(start_gps[j])
+#            end_gps.append(start_gps[j])
+#            obs_length.append(stop_gps[j]-start_gps[j])
+#            obs_pointing.append(pointings[j])
 
 
 
-
+## if the stop time of one obs, and the start time of the next obs are same, they are consevutive.
+## if they also have the pointing, let us combine them. To do this, create new start, stop list, 
+## with corresponding pointing lists
+#
+## remove first element
+#start_1 = start_gps[1:]
+#p_1 = pointings[1:]
+#
+## remove last element
+#stop_0 = stop_gps[:-1]
+#p_0 = pointings[:-1]
+#
+##consecutive_obs = np.where(np.asarray(start_1) == np.asarray(stop_0))
+##consecutive_poi =  np.where(np.asarray(p_1) == np.asarray(p_0)) 
+##
+##print(len(consecutive_obs[0]))
+##print(len(consecutive_poi[0]))
+#
+#good_idx = np.where((np.asarray(start_1) == np.asarray(stop_0)) & (np.asarray(p_1) == np.asarray(p_0)))[0]
+#
+#
+##for i in good_idx:
+##    print(i)
+#
+#
+#def ranges(nums):
+#    nums = sorted(set(nums))
+#    gaps = [[s, e] for s, e in zip(nums, nums[1:]) if s+1 < e]
+#    edges = iter(nums[:1] + sum(gaps, []) + nums[-1:])
+#    return list(zip(edges, edges))
+#
+#cont_ranges = ranges(good_idx)
+#
+#
+#for c in cont_ranges:
+#    if c[0] != c[1]:
+#        t1 = start_gps[c[0]]
+#        t2 = stop_gps[c[1]+1]
+#        del_t = t2 - t1
+#        p = pointings[c[0]]
+#        begin_gps.append(t1)
+#        end_gps.append(t2)
+#        obs_length.append(del_t)
+#        obs_pointing.append(p)
+#    else:
+#        t1 = start_gps[c[0]]
+#        t2 = stop_gps[c[1]]
+#        del_t = t2 - t1
+#        p = pointings[c[0]]
+#        begin_gps.append(t1)
+#        end_gps.append(t2)
+#        obs_length.append(del_t)
+#        obs_pointing.append(p)
+#
+#begin_gps, end_gps, obs_pointing, obs_length = zip(*sorted(zip(begin_gps, end_gps, obs_pointing, obs_length )))
+#
+#for i in range(len(begin_gps)):
+#    print(f'{obs_pointing[i]}: {begin_gps[i]}: {end_gps[i]}: {obs_length[i]}')
