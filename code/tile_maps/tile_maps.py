d01ea2649a063b50692db5e81c13b279cdc08026
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-17 14:00:45 +1100

    Read in channel map dict

--- code/tile_maps/tile_maps.py
+++ code/tile_maps/tile_maps.py
@@ -13,9 +13,6 @@ import sat_ids
 sys.path.append('../sat_channels')
 from sat_channels import time_tree, savgol_interp, time_filter
 
-sys.path.append('../null_test')
-from project_pass_healpix import 
-
 # Custom spectral colormap
 sys.path.append('../decode_rf_data')
 from colormap import spectral
@@ -28,8 +25,6 @@ if __name__=='__main__':
         Project a sat pass onto a healpix map using ephemeris data
         """)
     
-    parser.add_argument('--start_date', metavar='\b', help='Date from which to start aligning data. Ex: 2019-10-10')
-    parser.add_argument('--stop_date', metavar='\b', help='Date until which to align data. Ex: 2019-10-11')
 
     parser.add_argument(
             '--align_dir', metavar='\b',default='../../outputs/align_data/',
@@ -47,6 +42,8 @@ if __name__=='__main__':
             '--chrono_dir', metavar='\b', default='./../../outputs/sat_ephemeris/chrono_json',
             help='Output directory. Default=./../../outputs/sat_ephemeris/chrono_json/')
 
+    parser.add_argument('--start_date', metavar='\b', help='Date from which to start aligning data. Ex: 2019-10-10')
+    parser.add_argument('--stop_date', metavar='\b', help='Date until which to align data. Ex: 2019-10-11')
     parser.add_argument('--noi_thresh', metavar='\b', default=3,help='Noise Threshold: Multiples of MAD. Default=3.')
     parser.add_argument('--sat_thresh', metavar='\b', default=3,help='3 σ threshold to detect sats Default=3.')
     parser.add_argument('--nside', metavar='\b', default=32,help='Healpix Nside. Default = 32')
@@ -59,14 +56,15 @@ if __name__=='__main__':
     stop_date =         args.stop_date
     out_dir =           args.out_dir
     chan_map =          args.chan_map
-    savgol_window =     args.savgol_window
-    polyorder =         args.polyorder
-    interp_type =       args.interp_type
-    interp_freq =       args.interp_freq
     noi_thresh =        args.noi_thresh
     sat_thresh =        args.sat_thresh
     nside =             args.nside
 
     # Import list of Norad catalogue IDs
     sat_list = [id for id in sat_ids.norad_ids.values()]
-    print(sat_list)
+    
+    # Read channel map file
+    with open(chan_map) as map:
+        channel_map = json.load(map)
+
+    print(channel_map)
