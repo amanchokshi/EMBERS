9178b38496fdf619cb598595a33b59bd27d24d3f
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-17 14:03:40 +1100

    Back to original download format

--- code/beam_pointings/download_pointings.sh
+++ code/beam_pointings/download_pointings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p ./../../outputs/beam_pointings/full_set
+mkdir -p ./../../outputs/beam_pointings
 
 for i in {001..$1}
 do
-    wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam_pointings/full_set/pointings_$i.json
+    wget "http://ws.mwatelescope.org/metadata/find?mintime=$2&maxtime=$3&extended=1&page=$i" -O ./../../outputs/beam_pointings/pointings_$i.json
     sleep 28
 done 
