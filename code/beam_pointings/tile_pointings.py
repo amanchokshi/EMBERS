afb41a43d63ac7b0b5a18f60fd64f3428087119a
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-03-18 18:00:37 +1100

    Plotting function works

--- code/beam_pointings/tile_pointings.py
+++ code/beam_pointings/tile_pointings.py
@@ -2,7 +2,9 @@ import sys
 import json
 import argparse
 import numpy as np
+import seaborn as sns
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 sys.path.append('../decode_rf_data')
 from rf_data import tile_names
@@ -13,6 +15,8 @@ from sat_channels import time_tree
 
 def point_int(tile, start, stop, point_0, point_2, point_4):
     
+    # increment this by 0.5, for every obs which is in point_list. 
+    # 0.5 = 30 min
     p_0 = 0
     p_2 = 0
     p_4 = 0
@@ -34,6 +38,32 @@ def point_int(tile, start, stop, point_0, point_2, point_4):
                 else:
                     pass
     return [p_0, p_2, p_4]
+
+def plt_pointing_hist(sub=None, int_dict=None, tile=None):
+
+    ax = fig.add_subplot(sub)
+    
+    int_list = int_dict[tile]
+    x = range(len(int_list))
+    leg = int_list
+
+    pal = sns.cubehelix_palette(len(int_list), start=.4, rot=-.5, dark=.4, reverse=True)
+    barplot = plt.bar(x, int_list, color=sns.color_palette(pal))
+    
+    def autolabel(rects):
+        for idx,rect in enumerate(barplot):
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2., height, 
+                    leg[idx], ha='center', va='bottom', rotation=0)
+    
+    autolabel(barplot)
+    
+    plt.xticks(x, ['0','2','4'])
+    plt.ylabel('Hours')
+    plt.xlim(-0.7,len(int_list)-0.3)
+    plt.xlabel('MWA Grid Pointing Number')
+    plt.title(f'{tile}: Integration at MWA Grid Pointings')
+
 
 
 if __name__=='__main__':
@@ -69,7 +99,12 @@ if __name__=='__main__':
         p_integration = point_int(tile, start_date, stop_date, point_0, point_2, point_4)
         tile_integration[f'{tile}'] = p_integration
 
-    print(tile_integration)
+
+    plt.style.use('seaborn')
+    fig = plt.figure(figsize=(8,10))
+    ax = plt_pointing_hist(sub=111, int_dict=tile_integration, tile='rf0XX')
+    plt.tight_layout()
+    fig.savefig('test.png')
 
 
 
