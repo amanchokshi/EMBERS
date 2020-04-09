3ce2f04f7c20e3dd15898ff2a5ce720cf61fd47f
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-04-09 20:47:51 +1000

    Fixed argparse description

--- code/tile_maps/tile_maps.py
+++ code/tile_maps/tile_maps.py
@@ -376,7 +376,7 @@ if __name__=='__main__':
     parser.add_argument('--start_date', metavar='\b', help='Date from which to start aligning data. Ex: 2019-10-10')
     parser.add_argument('--stop_date', metavar='\b', help='Date until which to align data. Ex: 2019-10-11')
     parser.add_argument('--noi_thresh', metavar='\b', type=int, default=3,help='Noise Threshold: Multiples of MAD. Default=3.')
-    parser.add_argument('--sat_thresh', metavar='\b', type=int, default=1,help='3 σ threshold to detect sats Default=1.')
+    parser.add_argument('--sat_thresh', metavar='\b', type=int, default=1,help='σ threshold to detect sats Default=1.')
     parser.add_argument('--pow_thresh', metavar='\b', type=int, default=5,help='Power Threshold to detect sats. Default=10 dB.')
     parser.add_argument('--nside', metavar='\b', type=int,  default=32,help='Healpix Nside. Default = 32')
     parser.add_argument('--plots', metavar='\b', default=False,help='If True, create a gazzillion plots for each sat pass. Default = False')
