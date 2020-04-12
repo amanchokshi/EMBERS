405fa6549240c14663b7b50ffc933ee3b9ea4ccf
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-04-12 12:57:08 +1000

    Sat_maps function is parallelized and works now

--- code/tile_maps/plot_healpix.py
+++ code/tile_maps/plot_healpix.py
@@ -208,8 +208,9 @@ def good_maps(f):
         plt.close()
 
 
-def sat_maps(f):
+def sat_maps(sat):
     
+    f = Path(f'{map_dir}/S08XX_rf0XX_sat_maps.npz')
     f_name, _ = f.name.split('.')
     tile, ref, _, _ = f_name.split('_')
    
@@ -224,15 +225,14 @@ def sat_maps(f):
     ratio_map = tile_data['ratio_map']  
     
     for p in pointings:
-        for s in sat_ids:
 
-            fig = plt.figure(figsize=(8,10))
-            fig.suptitle(f'Satellite [{s}]: {tile}/{ref} @ {p}', fontsize=16)
-            ratio_sat_med = [(np.median(i) if i != [] else np.nan ) for i in tile_data['ratio_map'][p][s]]
-            ratio_sat_scaled = np.asarray([(i - np.nanmax(ratio_sat_med[:5000])) for i in ratio_sat_med])
-            plot_healpix(data_map=ratio_sat_scaled, sub=(1,1,1), cmap=jade)
-            plt.savefig(f'{out_dir}/sat_maps/{p}/{s}_{p}_{tile}_{ref}_passes.png',bbox_inches='tight')
-            plt.close()
+        fig = plt.figure(figsize=(8,10))
+        fig.suptitle(f'Satellite [{sat}]: {tile}/{ref} @ {p}', fontsize=16)
+        ratio_sat_med = [(np.median(i) if i != [] else np.nan ) for i in tile_data['ratio_map'][p][sat]]
+        ratio_sat_scaled = np.asarray([(i - np.nanmax(ratio_sat_med[:5000])) for i in ratio_sat_med])
+        plot_healpix(data_map=ratio_sat_scaled, sub=(1,1,1), cmap=jade)
+        plt.savefig(f'{out_dir}/sat_maps/{p}/{sat}_{p}_{tile}_{ref}_passes.png',bbox_inches='tight')
+        plt.close()
 
 
 
@@ -241,9 +241,9 @@ if __name__=='__main__':
     import argparse
     import matplotlib
     matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
     from pathlib import Path
     import concurrent.futures
+    import matplotlib.pyplot as plt
     
     import sys
     sys.path.append('../decode_rf_data')
@@ -269,7 +269,11 @@ if __name__=='__main__':
     map_dir = Path(args.map_dir)
     nside   = args.nside
     
+    # list of all possible satellites
+    sat_ids = list(norad_ids.values())
+    
     pointings = ['0','2','4']
+    
     map_files = [item for item in map_dir.glob('*.npz')]
 
     # Create output directory tree
@@ -285,10 +289,16 @@ if __name__=='__main__':
 #        results = executor.map(map_plots, map_files)
 
     # Parallization magic happens here
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = executor.map(good_maps, map_files)
+    #with concurrent.futures.ProcessPoolExecutor() as executor:
+    #    results = executor.map(good_maps, map_files)
 
     # plot maps for all sats, for only one tile_ref pair
     # S08XX has good time coverage
-    sat_maps(Path(f'{map_dir}/S08XX_rf0XX_sat_maps.npz'))
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(sat_maps, sat_ids)
+    #sat_maps(Path(f'{map_dir}/S08XX_rf0XX_sat_maps.npz'))
+
+#    for sat in sat_ids:
+#        sat_maps(sat)
 
