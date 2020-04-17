f98571d31e04bb269d610370186431e51055c65b
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-04-17 22:17:30 +1000

    Plot sat maps using raw time map data

---
+++ code/tile_maps/plot_sat_maps.py
@@ -0,0 +1,142 @@
+import sys
+import numpy as np
+import healpy as hp
+from scipy.stats import median_absolute_deviation as mad
+
+sys.path.append('../sat_ephemeris')
+from sat_ids import norad_ids
+
+def plot_healpix(data_map=None,sub=None,title=None,vmin=None,vmax=None,cmap=None):
+    '''Yeesh do some healpix magic to plot the thing'''
+    
+    # Healpix plotting script adapted from Dr. Jack Line's code
+    # https://github.com/JLBLine/MWA_ORBCOMM
+    
+    # Disable cryptic healpy warnings. Can't figure out where they originate
+    import warnings
+    warnings.filterwarnings("ignore", category=RuntimeWarning) 
+    
+    if vmin == None:
+        if cmap == None:
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,
+                    title=title,rot=(0,90,0),
+                    sub=sub,notext=True, return_projected_map=True)
+        else:
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,
+                    title=title,rot=(0,90,0), sub=sub,cmap=cmap,
+                    notext=True,return_projected_map=True)
+    else:
+        if cmap == None:
+            half_sky = hp.orthview(
+                    map=data_map,coord='E'
+                    ,half_sky=True,xsize=400,rot=(0,90,0),
+                    title=title,sub=sub,min=vmin,max=vmax,
+                    notext=True,return_projected_map=True)
+        else:
+            half_sky = hp.orthview(
+                    map=data_map,coord='E',
+                    half_sky=True,xsize=400,rot=(0,90,0),
+                    title=title,sub=sub,min=vmin,max=vmax,
+                    cmap=cmap,notext=True,return_projected_map=True)
+
+    hp.graticule(dpar=10,coord='E',color='k',alpha=0.3,dmer=45)
+   
+    # Altitude grid
+    hp.projtext(0.0*(np.pi/180.0), 0.0, '0', coord='E')
+    hp.projtext(30.0*(np.pi/180.0), 0.0, '30', coord='E')
+    hp.projtext(60.0*(np.pi/180.0), 0.0, '60', coord='E')
+
+    # Azimuth grid
+    hp.projtext(90.0*(np.pi/180.0), 00.0*(np.pi/180.0), r'$0^\circ$', coord='E',color='k',verticalalignment='top', fontsize=12)
+    hp.projtext(90.0*(np.pi/180.0), 90.0*(np.pi/180.0), r'$90^\circ$', coord='E',color='k',horizontalalignment='right', fontsize=12)
+    hp.projtext(90.0*(np.pi/180.0), 180.0*(np.pi/180.0), r'$180^\circ$', coord='E',color='k', fontsize=12)
+    hp.projtext(90.0*(np.pi/180.0), 270.0*(np.pi/180.0), r'$270^\circ$', coord='E',color='k', fontsize=12)
+    
+    # NSEW 
+    hp.projtext(90.0*(np.pi/180.0), 045.0*(np.pi/180.0), r'$N  $', coord='E',color='k',verticalalignment='top', horizontalalignment='right', fontsize=14)
+    hp.projtext(90.0*(np.pi/180.0), 135.0*(np.pi/180.0), r'$E  $', coord='E',color='k',horizontalalignment='right', fontsize=14)
+    hp.projtext(90.0*(np.pi/180.0), 225.0*(np.pi/180.0), r'$S  $', coord='E',color='k', fontsize=14)
+    hp.projtext(90.0*(np.pi/180.0), 315.0*(np.pi/180.0), r'$W  $', coord='E',color='k', verticalalignment='top', horizontalalignment='left', fontsize=14)
+
+
+def sat_maps(sat):
+    
+    f = Path(f'{map_dir}/S08XX_rf0XX_sat_maps.npz')
+    f_name, _ = f.name.split('.')
+    tile, ref, _, _ = f_name.split('_')
+   
+    pointings = ['0','2','4']
+
+    # list of all possible satellites
+    sat_ids = list(norad_ids.values())
+    
+    # load data from map .npz file
+    tile_data = np.load(f, allow_pickle=True)
+    tile_data = {key:tile_data[key].item() for key in tile_data}
+    tile_map = tile_data['tile_map']  
+    
+    for p in pointings:
+
+        fig = plt.figure(figsize=(8,10))
+        fig.suptitle(f'Satellite [{sat}]: {tile}/{ref} @ {p}', fontsize=16)
+        tile_sat_med = [(np.median(i) if i != [] else np.nan ) for i in tile_data['tile_map'][p][sat]]
+        #tile_sat_scaled = np.asarray([(i - np.nanmax(tile_sat_med[:5000])) for i in tile_sat_med])
+        plot_healpix(data_map=np.asarray(tile_sat_med), sub=(1,1,1), cmap=jade)
+        plt.savefig(f'{out_dir}/sat_maps/{p}/{sat}_{p}_{tile}_{ref}_passes.png',bbox_inches='tight')
+        plt.close()
+
+
+
+if __name__=='__main__':
+    
+    import argparse
+    import matplotlib
+    matplotlib.use('Agg')
+    from pathlib import Path
+    import concurrent.futures
+    import matplotlib.pyplot as plt
+    
+    import sys
+    sys.path.append('../decode_rf_data')
+    from colormap import spectral, jade, kelp
+    
+    # Custom spectral colormap
+    jade, _ = jade()
+    
+
+    parser = argparse.ArgumentParser(description="""
+        Plot healpix map of reference data
+        """)
+    
+    parser.add_argument('--out_dir', metavar='\b', default='./../../outputs/tile_maps/',help='Output directory. Default=./../../outputs/tile_maps/')
+    parser.add_argument('--map_dir', metavar='\b', default='./../../outputs/tile_maps/tile_maps_raw',help='Output directory. Default=./../../outputs/tile_maps/tile_maps_raw')
+    parser.add_argument('--nside', metavar='\b', type=int,  default=32,help='Healpix Nside. Default = 32')
+    
+    args = parser.parse_args()
+    
+    out_dir = Path(args.out_dir)
+    map_dir = Path(args.map_dir)
+    nside   = args.nside
+    
+    # list of all possible satellites
+    sat_ids = list(norad_ids.values())
+    
+    pointings = ['0','2','4']
+    
+    map_files = [item for item in map_dir.glob('*.npz')]
+
+    # Create output directory tree
+    for p in pointings:
+        Path(f'{out_dir}/sat_maps/{p}/').mkdir(parents=True, exist_ok=True)
+
+    # plot maps for all sats, for only one tile_ref pair
+    # S08XX has good time coverage
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(sat_maps, sat_ids)
+
+
+
