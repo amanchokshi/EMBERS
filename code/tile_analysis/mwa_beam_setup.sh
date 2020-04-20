a0b1df3e7fd4759a63d3a1dc3d342992e9f042b6
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-04-20 19:28:33 +1000

    Git clone mwa_pd repo, and download FEE beam model into correct directory

---
+++ code/tile_analysis/mwa_beam_setup.sh
@@ -0,0 +1,3 @@
+mkdir ./../../../MWA_Beam/
+git clone https://github.com/MWATelescope/mwa_pb.git ./../../../MWA_Beam/
+wget http://cerberus.mwa128t.org/mwa_full_embedded_element_pattern.h5 -P ./../../../MWA_Beam/mwa_pb/mwa_pb/data
