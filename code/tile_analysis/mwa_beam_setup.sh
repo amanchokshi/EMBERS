15a825289e2461acdfef92a28eeb95c4e76ead3a
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-04-22 22:20:49 +1000

    Moved mwa_beam_setup into setup.sh because Im now using a local conda env

--- code/tile_analysis/mwa_beam_setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir ./../../../MWA_Beam/
-git clone https://github.com/MWATelescope/mwa_pb.git ./../../../MWA_Beam/
-wget http://cerberus.mwa128t.org/mwa_full_embedded_element_pattern.h5 -P ./../../../MWA_Beam/mwa_pb/data
-cd ./../../../MWA_Beam/
-python setup.py install
-cd ./../mwa-satellites/code/tile_analysis
-
