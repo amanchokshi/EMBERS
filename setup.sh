21ff7976c15f62a0c551d7bc48bfc6835ffe45e9
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-04-22 22:44:19 +1000

    Cleaned up setip

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # Create local conda env installation
+conda deactivate
 conda env create python=3.7 -f env.yml --prefix ./beam-env
 conda config --set env_prompt '({name})'
 
@@ -7,9 +8,10 @@ conda activate ./beam-env
 
 # install mwa primary beam repo and download FEE data
 mkdir ./beam-env/lib/python3.7/site-packages/MWA_PB/
-git clone https://github.com/MWATelescope/mwa_pb.git ./beam-env/lib/python3.7/site-packages/MWA_PB/
-wget http://cerberus.mwa128t.org/mwa_full_embedded_element_pattern.h5 -P ./beam-env/lib/python3.7/site-packages/MWA_PB/mwa_pb/data
 cd ./beam-env/lib/python3.7/site-packages/MWA_PB/
+
+git clone https://github.com/MWATelescope/mwa_pb.git .
+wget http://cerberus.mwa128t.org/mwa_full_embedded_element_pattern.h5 -P ./mwa_pb/data
 python setup.py install
 conda deactivate
 cd ../../../../../
