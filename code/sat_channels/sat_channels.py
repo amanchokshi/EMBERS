acab6118a4af9cb6c719f7dcbe7035a35eb52b6e
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-05-04 15:50:59 +1000

    Changed power threshold to 15dBm

--- code/sat_channels/sat_channels.py
+++ code/sat_channels/sat_channels.py
@@ -304,7 +304,7 @@ if __name__=="__main__":
     parser.add_argument('--chrono_dir', metavar='\b', default='./../../outputs/sat_ephemeris/chrono_json',help='Output directory. Default=./../../outputs/sat_ephemeris/chrono_json/')
     parser.add_argument('--noi_thresh', metavar='\b', type=int, default=3,help='Noise Threshold: Multiples of MAD. Default=3.')
     parser.add_argument('--sat_thresh', metavar='\b', type=int, default=1,help='1 σ threshold to detect sats Default=1.')
-    parser.add_argument('--pow_thresh', metavar='\b', type=int, default=20,help='Power Threshold to detect sats. Default=20 dB.')
+    parser.add_argument('--pow_thresh', metavar='\b', type=int, default=15,help='Power Threshold to detect sats. Default=15 dB.')
     parser.add_argument('--occ_thresh', metavar='\b', type=int, default=0.80,help='Occupation Threshold of sat in window. Default=0.80')
 
     
