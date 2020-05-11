ade6a6b483baa034c115f41e527d807458f384d1
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-05-11 19:52:57 +1000

    Figuring out fits structure

--- code/beam_pointings/dead_dipoles.py
+++ code/beam_pointings/dead_dipoles.py
@@ -9,40 +9,23 @@ hdu = fits.open(meta)
 obsid = hdu[0].header['GPSTIME']
 delays = hdu[1].data['Delays']
 
-print(repr(hdu[0].header))
+tile_names = hdu[1].data['TileName']
+delays = hdu[1].data['Delays']
+pols = hdu[1].data['Pol']
 
-## Counts the number of flagged dipoles for each tile
-#flags = []
-#for i in range(delays.shape[0]):
-#    flags.append(np.count_nonzero(delays[i] == 32))
-#
-#flags = np.array(flags)
-## Seperates XX, YY tiles
-#xx = flags[1::2]
-#yy = flags[::2]
-#xx_1 = np.count_nonzero(xx == 1)
-#xx_2 = np.count_nonzero(xx == 2)
-#yy_1 = np.count_nonzero(yy == 1)
-#yy_2 = np.count_nonzero(yy == 2)
-#metadata = [obsid, xx_1, xx_2, yy_1, yy_2]
-#data.append(metadata)
-#
 tiles = [
-        'HexS6',
-        'HexS7',
-        'HexS8',
-        'HexS9',
-        'HexS10',
-        'HexS12',
-        'HexS29',
-        'HexS30',
-        'HexS31',
-        'HexS32',
-        'HexS33',
-        'HexS34',
-        'HexS35',
-        'HexS36'
+        'HexS6', 'HexS7',
+        'HexS8', 'HexS9',
+        'HexS10', 'HexS12',
+        'HexS29', 'HexS30',
+        'HexS31', 'HexS32',
+        'HexS33', 'HexS34', 
+        'HexS35', 'HexS36'
         ]
+
+for t in tiles:
+    idx = np.where(tile_names == t)
+    print(tile_names[idx])
 
 
 
