ed4b2dd3453cd66ad983056f52f093335c0b1996
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-05-11 11:55:16 +1000

    fixed typo

--- code/tile_maps/clip_test.sh
+++ code/tile_maps/clip_test.sh
@@ -6,7 +6,7 @@ p compare_beams.py --out_dir=./../../outputs/tile_maps/40dBm/compare_beams/ --ma
 
 echo "-30dBm test"
 p tile_maps.py --start_date=2019-09-12 --stop_date=2020-03-16 --out_dir=./../../outputs/tile_maps/30dBm/tile_maps_raw/ --rfe_clip=-30
-p tile_maps_norm.py --out_dir=./../../outputs/tile_maps/30dBm/tile_maps_norm/ --map_dir=./../../outputs/tile_maps/20dBm/tile_maps_raw
+p tile_maps_norm.py --out_dir=./../../outputs/tile_maps/30dBm/tile_maps_norm/ --map_dir=./../../outputs/tile_maps/30dBm/tile_maps_raw
 p plot_tile_maps.py --out_dir=./../../outputs/tile_maps/30dBm --map_dir=./../../outputs/tile_maps/30dBm/tile_maps_norm
 p compare_beams.py --out_dir=./../../outputs/tile_maps/30dBm/compare_beams/ --map_dir=../../outputs/tile_maps/30dBm/tile_maps_norm/
 
