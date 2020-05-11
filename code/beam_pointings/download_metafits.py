8cc9eea20b61c3f3564cd655131415002dc085b3
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-05-11 18:27:54 +1000

    Download every 10th relevant metafits file

--- code/beam_pointings/download_metafits.py
+++ code/beam_pointings/download_metafits.py
@@ -1,4 +1,6 @@
 import json
+import wget
+import time
 import argparse
 import numpy as np
 from pathlib import Path
@@ -14,8 +16,8 @@ parser.add_argument(
         help='Directory where json metadata files live. Default=./../../outputs/beam_pointings/ultimate_pointing_times.json')
 
 parser.add_argument(
-        '--out_dir', metavar='\b', default='./../../outputs/metafits/', 
-        help='Directory where json metadata files live. Default=./../../outputs/metafits/')
+        '--out_dir', metavar='\b', default='./../../outputs/beam_pointings/metafits/', 
+        help='Directory where json metadata files live. Default=./../../outputs/beam_pointings/metafits/')
 
 args = parser.parse_args()
 obs_ids     = args.obs_ids
@@ -26,8 +28,14 @@ out_dir.mkdir(parents=True, exist_ok=True)
 with open(obs_ids) as gps:
     gps_times = json.load(gps)['start_gps']
 
-for i in gps_times[::10]:
-    print(i)
+cerberus_url = 'http://ws.mwatelescope.org/metadata/fits?obs_id='
+
+for i in gps_times[10::10]:
+    print(f'Downloading {i} metafits')
+    wget.download(f'{cerberus_url}{i}', f'{out_dir}/{i}.metafits')
+    print('\n')
+    time.sleep(10)   
+    
 
 
 
