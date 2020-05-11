8a3350403e0128cc6cde9176fadb836e3793f4e5
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-05-11 20:23:49 +1000

    Create dictionary with tile keys and flagged dipole values

--- code/beam_pointings/dead_dipoles.py
+++ code/beam_pointings/dead_dipoles.py
@@ -23,9 +23,32 @@ tiles = [
         'HexS35', 'HexS36'
         ]
 
+flags = {}
+
+for t in tiles:
+    for p in ['X', 'Y']:
+        flags[f'{t}{p}'] = []
+
+
 for t in tiles:
     idx = np.where(tile_names == t)
-    print(tile_names[idx])
+    for i in range(2):
+        idx_p = idx[0][i]
+        
+        t_name = tile_names[idx_p]
+        t_pol = pols[idx_p]
+        t_del = delays[idx_p]
+        t_flag = np.where(t_del == 32)[0] + 1
+         
+        if t_flag.size != 0:
+            print(t_name, t_pol, t_del, t_flag[0])
+            flags[f'{t_name}{t_pol}'].append(t_flag[0])
+        else:
+            print(t_name, t_pol, t_del, 0)
+            flags[f'{t_name}{t_pol}'].append(0)
+
+print(flags)
+
 
 
 
