0c1fc4c97bb7b20b61dc95fd10fe70c2a442a330
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-05-11 14:22:51 +1000

    Download metafits files to determine flagging information

---
+++ code/beam_pointings/download_metafits.py
@@ -0,0 +1,32 @@
+import json
+import argparse
+import numpy as np
+from pathlib import Path
+
+
+parser = argparse.ArgumentParser(description="""
+        Download metafits files to find
+        dipole flagging of tiles
+        """)
+
+parser.add_argument(
+        '--obs_ids', metavar='\b', default='./../../outputs/beam_pointings/ultimate_pointing_times.json', 
+        help='Directory where json metadata files live. Default=./../../outputs/beam_pointings/ultimate_pointing_times.json')
+
+parser.add_argument(
+        '--out_dir', metavar='\b', default='./../../outputs/metafits/', 
+        help='Directory where json metadata files live. Default=./../../outputs/metafits/')
+
+args = parser.parse_args()
+obs_ids     = args.obs_ids
+out_dir     = Path(args.out_dir)
+
+
+with open(obs_ids) as gps:
+    gps_times = json.load(gps)['start_gps']
+
+print(len(gps_times[::10]))
+
+
+
+
