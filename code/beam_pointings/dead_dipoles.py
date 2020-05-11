bc58bc6c782177ab24f5f51cdc796cc56a7095ff
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-05-11 20:36:22 +1000

    Added argparse, shoved stuff into functions

--- code/beam_pointings/dead_dipoles.py
+++ code/beam_pointings/dead_dipoles.py
@@ -1,18 +1,28 @@
+import json
+import argparse
 import numpy as np
-from astropy.io import fits
 from pathlib import Path
+from astropy.io import fits
 
-meta = Path('./1252145824.metafits')
+parser = argparse.ArgumentParser(description="""
+        Look for dead dipoles
+        """)
 
+parser.add_argument(
+        '--metafits', metavar='\b', default='./../../outputs/beam_pointings/metafits/', 
+        help='Directory where metafits files live. Default=./../../outputs/beam_pointings/metafits/')
 
-hdu = fits.open(meta)
-obsid = hdu[0].header['GPSTIME']
-delays = hdu[1].data['Delays']
+parser.add_argument(
+        '--output', metavar='\b', default='./../../outputs/beam_pointings/flags.json', 
+        help='Directory where metafits files live. Default=./../../outputs/beam_pointings/flags.json')
 
-tile_names = hdu[1].data['TileName']
-delays = hdu[1].data['Delays']
-pols = hdu[1].data['Pol']
+args = parser.parse_args()
+metafits     = Path(args.metafits)
+output       = Path(args.output)
 
+meta_files = [item for item in metafits.glob('*.metafits')]
+
+# list of all tiles used in this experiment
 tiles = [
         'HexS6', 'HexS7',
         'HexS8', 'HexS9',
@@ -23,29 +33,43 @@ tiles = [
         'HexS35', 'HexS36'
         ]
 
+# dictionary to save flag info.
+# 0 indicates no dead dipole
+# 1-16 dipole index
 flags = {}
-
 for t in tiles:
     for p in ['X', 'Y']:
         flags[f'{t}{p}'] = []
 
 
-for t in tiles:
-    idx = np.where(tile_names == t)
-    for i in range(2):
-        idx_p = idx[0][i]
-        
-        t_name = tile_names[idx_p]
-        t_pol = pols[idx_p]
-        t_del = delays[idx_p]
-        t_flag = np.where(t_del == 32)[0] + 1
-         
-        if t_flag.size != 0:
-            print(t_name, t_pol, t_del, t_flag[0])
-            flags[f'{t_name}{t_pol}'].append(t_flag[0])
-        else:
-            print(t_name, t_pol, t_del, 0)
-            flags[f'{t_name}{t_pol}'].append(0)
+def find_flag(meta):
+    hdu = fits.open(meta)
+    obsid = hdu[0].header['GPSTIME']
+    delays = hdu[1].data['Delays']
+    
+    tile_names = hdu[1].data['TileName']
+    delays = hdu[1].data['Delays']
+    pols = hdu[1].data['Pol']
+    
+    
+    
+    for t in tiles:
+        idx = np.where(tile_names == t)
+        for i in range(2):
+            idx_p = idx[0][i]
+            
+            t_name = tile_names[idx_p]
+            t_pol = pols[idx_p]
+            t_del = delays[idx_p]
+            t_flag = np.where(t_del == 32)[0] + 1
+             
+            if t_flag.size != 0:
+                flags[f'{t_name}{t_pol}'].append(t_flag[0])
+            else:
+                flags[f'{t_name}{t_pol}'].append(0)
+
+for m in meta_files:
+    find_flag(m)
 
 print(flags)
 
