418e45448f18aac278029de879305dc597ed06fe
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-05-11 19:28:08 +1000

    Analyse number of dead dipoles for tiles

---
+++ code/beam_pointings/dead_dipoles.py
@@ -0,0 +1,46 @@
+from astropy.io import fits
+import numpy as np
+
+meta = '~/Downloads/1252145824.metafits'
+
+hdu = fits.open(meta)
+obsid = hdu[0].header['GPSTIME']
+delays = hdu[1].data['Delays']
+
+print(delays)
+
+## Counts the number of flagged dipoles for each tile
+#flags = []
+#for i in range(delays.shape[0]):
+#    flags.append(np.count_nonzero(delays[i] == 32))
+#
+#flags = np.array(flags)
+## Seperates XX, YY tiles
+#xx = flags[1::2]
+#yy = flags[::2]
+#xx_1 = np.count_nonzero(xx == 1)
+#xx_2 = np.count_nonzero(xx == 2)
+#yy_1 = np.count_nonzero(yy == 1)
+#yy_2 = np.count_nonzero(yy == 2)
+#metadata = [obsid, xx_1, xx_2, yy_1, yy_2]
+#data.append(metadata)
+#
+tiles = [
+        'HexS6',
+        'HexS7',
+        'HexS8',
+        'HexS9',
+        'HexS10',
+        'HexS12',
+        'HexS29',
+        'HexS30',
+        'HexS31',
+        'HexS32',
+        'HexS33',
+        'HexS34',
+        'HexS35',
+        'HexS36'
+        ]
+
+
+
