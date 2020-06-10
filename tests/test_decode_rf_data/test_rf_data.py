8befc4f3eb227d11da9431f51c34fd82ccfc3da0
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-06-10 22:11:30 +1000

    Removed old commented code

--- tests/test_decode_rf_data/test_rf_data.py
+++ tests/test_decode_rf_data/test_rf_data.py
@@ -3,9 +3,6 @@ import unittest
 sys.path.append('../../code/decode_rf_data')
 import rf_data as rf
 
-#power, times = rf.read_data('../../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
-#print(power.shape)
-#print(times.shape)
 
 class Test_rf_data(unittest.TestCase):
 
