aa7992d603555a5acd67ecce5fe8c20533f0d23c
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-06-10 23:20:28 +1000

    Working on batch waterfall tests

--- tests/test_decode_rf_data/test_batch_waterfall.py
+++ tests/test_decode_rf_data/test_batch_waterfall.py
@@ -1,59 +1,75 @@
 import sys
 import unittest
+import concurrent.futures
+from datetime import datetime, timedelta
 sys.path.append('../../code/decode_rf_data')
 import batch_waterfall as bf
-#import rf_data as rf
 from pathlib import Path
+import rf_data as rf
 
-rf_file = 'S06XX_2019-10-01-14:30'
-rf_dir = '../../data/rf_data/S06XX/2019-10-01'
-bf.plt_single_waterfall(rf_dir, rf_file, './')
+## Test plt_single_waterfall
+#rf_file = 'S06XX_2019-10-01-14:30'
+#rf_dir = '../../data/rf_data/S06XX/2019-10-01'
+#bf.plt_single_waterfall(rf_dir, rf_file, './')
 
+# Test waterfall_plot
+data_dir = '../../data/rf_data'
+start_date = '2019-10-01'
+stop_date = '2019-10-01'
+out_dir = './'
+
+tiles = rf.tile_names()
+t_start = datetime.strptime(start_date, '%Y-%m-%d')
+t_stop = datetime.strptime(stop_date, '%Y-%m-%d')
+n_days = (t_stop - t_start).days
+
+dates = []
+date_time = []
+
+for i in range(n_days+1):
+    day = t_start + timedelta(days=i)
+    date = day.strftime('%Y-%m-%d')
+    dates.append(date)
+    d_t = []
+
+    for j in range(48):
+        t_delta = datetime.strptime(date,'%Y-%m-%d') + timedelta(minutes=30*j)
+        d_time = t_delta.strftime('%Y-%m-%d-%H:%M')
+        d_t.append(d_time)
+
+        date_time.append(d_t)
+
+
+data_dir = Path(data_dir)
+out_dir = Path(out_dir)
+
+for tile in tiles:
+    for d in range(len(dates)):
+        rf_path = data_dir/tile/dates[d]
+        for t in date_time[d]:
+            rf_name = f'{tile}_{t}'
+            bf.waterfall_plot(t)
+        #with concurrent.futures.ProcessPoolExecutor() as executor:
+        #    results = executor.map(bf.waterfall_plot, date_time[d])
+
+            #for result in results:
+            #    print(result)
 
 class Test_batch_waterfall(unittest.TestCase):
 
     
-    def test_plt_single_waterfall(self):    
-        self.assertTrue(Path(f'{rf_file}.png').is_file())
-
-        if Path(f'{rf_file}.png').is_file() is True:
-            Path(f'{rf_file}.png').unlink()
-
-
-
-
-
-
-#    power, times = rf.read_data('../../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
+#    def test_plt_single_waterfall(self):    
+#        self.assertTrue(Path(f'{rf_file}.png').is_file())
 #
-#    # Test the shape of data and that the time & power arrays have the same length
-#    def test_read_data(self):    
-#        self.assertEqual(self.power.shape[0], self.times.shape[0])
-#        self.assertEqual(self.power.shape, (16655, 112))
-#
-#
-#    # Test tile names. Hardcoded in rf_data
-#    def test_tile_names(self):
-#            tiles = [
-#                    'rf0XX', 'rf0YY', 'rf1XX', 'rf1YY',
-#                    'S06XX', 'S06YY', 'S07XX', 'S07YY',
-#                    'S08XX', 'S08YY', 'S09XX', 'S09YY',
-#                    'S10XX', 'S10YY', 'S12XX', 'S12YY',
-#                    'S29XX', 'S29YY', 'S30XX', 'S30YY',
-#                    'S31XX', 'S31YY', 'S32XX', 'S32YY',
-#                    'S33XX', 'S33YY', 'S34XX', 'S34YY',
-#                    'S35XX', 'S35YY', 'S36XX', 'S36YY'
-#                    ]
-#            self.assertEqual(rf.tile_names(), tiles)
-#
-#
-#    # Test that a matplotlib plot figure is created
-#    def test_plot_waterfall(self):
-#        import matplotlib.pyplot as plt
-#        num_figures_before = plt.gcf().number
-#        plt = rf.plot_waterfall(self.power, self.times, None)
-#        num_figures_after = plt.gcf().number
-#        self.assertEqual(num_figures_before+1, num_figures_after)
+#        if Path(f'{rf_file}.png').is_file() is True:
+#            Path(f'{rf_file}.png').unlink()
+
+    def test_waterfall_plot(self):
+        pass
+
+
+
+
 
 
 if __name__ == "__main__":
