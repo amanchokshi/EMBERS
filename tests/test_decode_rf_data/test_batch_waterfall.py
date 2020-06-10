aaeb1d31d11ae9aa25240d81050853df89de9888
Aman Chokshi <aman.chokshi@student.unimelb.edu.au>
2020-06-11 00:27:22 +1000

    Test works by verifying that plot is created in correct dir structure, then the whole temp dir is deleted

--- tests/test_decode_rf_data/test_batch_waterfall.py
+++ tests/test_decode_rf_data/test_batch_waterfall.py
@@ -1,71 +1,44 @@
 import sys
+import shutil
 import unittest
 import concurrent.futures
+from itertools import repeat
 from datetime import datetime, timedelta
 sys.path.append('../../code/decode_rf_data')
 import batch_waterfall as bf
 from pathlib import Path
 import rf_data as rf
 
-## Test plt_single_waterfall
-#rf_file = 'S06XX_2019-10-01-14:30'
-#rf_dir = '../../data/rf_data/S06XX/2019-10-01'
-#bf.plt_single_waterfall(rf_dir, rf_file, './')
+# Test plt_single_waterfall
+rf_file = 'S06XX_2019-10-01-14:30'
+rf_dir = '../../data/rf_data/S06XX/2019-10-01'
+bf.plt_single_waterfall(rf_dir, rf_file, './')
 
 # Test waterfall_plot
-data_dir = '../../data/rf_data'
-start_date = '2019-10-01'
-stop_date = '2019-10-01'
+tile_name = 'rf0XX'
 out_dir = './'
-
-tiles = rf.tile_names()
-t_start = datetime.strptime(start_date, '%Y-%m-%d')
-t_stop = datetime.strptime(stop_date, '%Y-%m-%d')
-n_days = (t_stop - t_start).days
-
-dates = []
-date_time = []
-
-for i in range(n_days+1):
-    day = t_start + timedelta(days=i)
-    date = day.strftime('%Y-%m-%d')
-    dates.append(date)
-    d_t = []
-                                            
-    for j in range(48):
-        t_delta = datetime.strptime(date,'%Y-%m-%d') + timedelta(minutes=30*j)
-        d_time = t_delta.strftime('%Y-%m-%d-%H:%M')
-        d_t.append(d_time)
-                                                                                                
-        date_time.append(d_t)    
-
-data_dir = Path(data_dir)
-out_dir = Path(out_dir)
-
-for tile in tiles:
-    for d in range(len(dates)):
-        rf_path = data_dir/tile/dates[d]
-                                            
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            results = executor.map(bf.waterfall_plot, date_time[d])
-                                                                                           
-        for result in results:
-            print(result)
-
+data_dir = '../../data/rf_data'
+date = '2019-10-01'
+time_stamp = '2019-10-01-14:30'
+bf.waterfall_plot(tile_name, out_dir, data_dir, date, time_stamp)
 
 
 
 class Test_batch_waterfall(unittest.TestCase):
 
     
-#    def test_plt_single_waterfall(self):    
-#        self.assertTrue(Path(f'{rf_file}.png').is_file())
-#
-#        if Path(f'{rf_file}.png').is_file() is True:
-#            Path(f'{rf_file}.png').unlink()
+    def test_plt_single_waterfall(self):    
+        self.assertTrue(Path(f'{rf_file}.png').is_file())
+
+        if Path(f'{rf_file}.png').is_file() is True:
+            Path(f'{rf_file}.png').unlink()
 
     def test_waterfall_plot(self):
-        pass
+        rf_path = Path(f'waterfalls/{date}/{time_stamp}/{tile_name}_{time_stamp}.png')
+        self.assertTrue(rf_path.is_file())
+
+        if rf_path.is_file() is True:
+            shutil.rmtree(Path(f'waterfalls'))
 
 
 
