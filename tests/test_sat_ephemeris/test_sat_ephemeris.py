f61eb7dab93f519d66e5c7c2a7d124d59b5eef0a
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-12 00:40:29 +1000

    Working on testing sat_ephemeris

---
+++ tests/test_sat_ephemeris/test_sat_ephemeris.py
@@ -0,0 +1,29 @@
+import sys
+import unittest
+sys.path.append('../../code/sat_ephemeris')
+import sat_ephemeris as se
+
+
+class Test_sat_ephemeris(unittest.TestCase):
+
+    def test_load_tle(self):
+        pass
+
+    def test_epoch_ranges(self):
+        pass
+    
+    def test_epoch_time_array(self):
+        pass
+
+    def test_sat_pass(self):
+        pass
+        
+    def test_ephem_data(self):
+        pass
+    
+    def test_sat_plot(self):
+        pass
+        
+
+if __name__ == "__main__":
+    unittest.main()
