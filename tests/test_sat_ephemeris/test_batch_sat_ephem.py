ddbed7595ff3389bf05d882089373bf7dfc8c60c
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-12 17:41:29 +1000

    Test batch sat ephem

---
+++ tests/test_sat_ephemeris/test_batch_sat_ephem.py
@@ -0,0 +1,18 @@
+import sys
+import unittest
+sys.path.append('../../code/sat_ephemeris')
+import sat_ids
+
+
+#class Test_sat_ids(unittest.TestCase):
+#
+#    def test_norad_ids(self):    
+#        
+#        self.assertEqual(len(norad_dict.keys()), 73)
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
+
+
