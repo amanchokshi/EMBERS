2c0a6ce941f722574a109a618bd8fc55488c0ddc
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-13 21:13:05 +1000

    Plot ephem test passes

--- tests/test_sat_ephemeris/test_plot_ephemeris.py
+++ tests/test_sat_ephemeris/test_plot_ephemeris.py
@@ -1,21 +1,23 @@
 import sys
 import json
 import unittest
+from pathlib import Path
 sys.path.append('../../code/sat_ephemeris')
 import plot_ephemeris as pe
 
 
 class Test_plot_ephemeris(unittest.TestCase):
     
-    json_dir = './'
+    json_dir = '../../data/tests/ephem_json'
+    out_dir = './'
+    sat_id = 25986
         
     def test_plot_ephem(self):    
 
-        pe.plot_ephem(self.tle_dir, self.cadence, self.out_dir, self.sat_id)
-        out_file = Path(f'./{self.sat_id}.json')
+        pe.plot_ephem(self.json_dir, self.out_dir, self.sat_id)
+        out_file = Path(f'./{self.sat_id}.png')
 
         self.assertTrue(out_file.is_file())
-        self.assertEqual(sat_id_json[0], self.sat_id)
         out_file.unlink()
 
 
