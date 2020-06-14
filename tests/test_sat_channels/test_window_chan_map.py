932b73ab1fa8b53de6b6530f5ce1dc4d07682945
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-14 17:25:19 +1000

    Test window chan map module

---
+++ tests/test_sat_channels/test_window_chan_map.py
@@ -0,0 +1,30 @@
+import sys
+import unittest
+sys.path.append('../../code/sat_channels')
+import window_chan_map as wcm
+
+
+class Test_window_chan_map(unittest.TestCase):
+
+    def test_time_tree(self):
+        pass
+
+    def test_time_filter(self):
+        pass
+
+    def test_read_aligned(self):
+        pass
+
+    def test_noise_floor(self):
+        pass
+
+    def test_power_ephem(self):
+        pass
+
+    def test_window_chan_map(self):
+        pass
+
+        
+
+if __name__ == "__main__":
+    unittest.main()
