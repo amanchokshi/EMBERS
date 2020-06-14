5379326573d4621ccec9a53eb28b5f95546dc18d
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-15 02:09:50 +1000

    Modernized. function & f-string

--- code/beam_pointings/date_gps.py
+++ code/beam_pointings/date_gps.py
@@ -1,20 +1,20 @@
-import argparse
 from astropy.time import Time
-
-parser = argparse.ArgumentParser(description="""
-        Convert date-time [iso] to gps time
-        """)
 
 def convert_gps(date):
     return int(Time(date, format='iso').gps)
 
-parser.add_argument('--date', metavar='\b', help='Date-time to be converted to gps-seconds. Ex:2019-01-01 00:00:00')
-
-args = parser.parse_args()
-date = args.date
-
-gps = convert_gps(date)
-print(f'gps: {gps}')
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="""
+            Convert date-time [iso] to gps time
+            """)
+    parser.add_argument('--date', metavar='\b', help='Date-time to be converted to gps-seconds. Ex:2019-01-01 00:00:00')
+    
+    args = parser.parse_args()
+    date = args.date
+    
+    gps = convert_gps(date)
+    print(f'gps: {gps}')
 
 
 
