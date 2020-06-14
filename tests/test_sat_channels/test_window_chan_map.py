67096b994e57aec22c803194b48d34992e5fbc9b
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-15 00:49:14 +1000

    Tests for window_chan_maps passing

--- tests/test_sat_channels/test_window_chan_map.py
+++ tests/test_sat_channels/test_window_chan_map.py
@@ -1,7 +1,9 @@
 import sys
 import json
+import shutil
 import unittest
 import numpy as np
+from pathlib import Path
 sys.path.append('../../code/sat_channels')
 import window_chan_map as wcm
 sys.path.append('../../code/decode_rf_data')
@@ -9,7 +11,8 @@ from colormap import spectral
 
 
 class Test_window_chan_map(unittest.TestCase):
-
+    
+    # Window chan argparse variables
     start_date = '2019-10-01'
     stop_date = '2019-10-01'
     ali_npz = '../../data/tests/align_data/2019-10-01/2019-10-01-14:30/rf0XX_S06XX_2019-10-01-14:30_aligned.npz'
@@ -23,6 +26,7 @@ class Test_window_chan_map(unittest.TestCase):
     power, times = wcm.read_aligned(ali_file=ali_npz)
     power, noise_t = wcm.noise_floor(sat_thresh, noise_thresh, data=power)
     
+    # Test time filter
     with open(chrono_file) as chrono:
         chrono_ephem = json.load(chrono)
         norad_list = []
@@ -38,12 +42,42 @@ class Test_window_chan_map(unittest.TestCase):
 
         intvl = wcm.time_filter(rise_ephem, set_ephem, np.asarray(times))
 
-    good_chan = wcm.good_chans(
+    # Test good_chans fucntions with positive and null test cases
+    good_chan0 = wcm.good_chans(
             ali_npz, chrono_file, 
             norad_list[0], sat_thresh, 
             noise_thresh, pow_thresh, 
             occ_thresh, '2019-10-01', 
             '2019-10-01-14:30', False)
+    
+    good_chan1 = wcm.good_chans(
+            ali_npz, chrono_file, 
+            25414, sat_thresh, 
+            noise_thresh, pow_thresh, 
+            occ_thresh, '2019-10-01', 
+            '2019-10-01-14:30', False)
+
+        
+    ali_dir     = '../../data/tests/align_data'
+    chrono_dir  = '../../data/tests/chrono_json'
+    out_dir     = './'
+    obs_stamp   = ['2019-10-01', '2019-10-01-14:30']
+    plots       = False
+    plt_dir     = './chan_plots'
+
+    wcm.window_chan_map(
+            ali_dir,
+            chrono_dir,
+            sat_thresh,
+            noise_thresh,
+            pow_thresh,
+            occ_thresh,
+            out_dir,
+            plots, 
+            plt_dir, 
+            obs_stamp)
+
+
 
     def test_time_tree(self):
         self.assertEqual(self.start_date, self.dates[0])
@@ -65,12 +99,22 @@ class Test_window_chan_map(unittest.TestCase):
 
     def test_time_filter(self):
         self.assertEqual(self.intvl, [27, 621])
-#
-#    def test_good_chans(self):
-#        pass
-#
-#    def test_window_chan_map(self):
-#        pass
+
+    def test_good_chans(self):
+        self.assertEqual(self.good_chan0, 0)
+        self.assertEqual(self.good_chan1, 41)
+
+    def test_window_chan_map(self):
+        chan_json = Path('window_maps/2019-10-01-14:30.json')
+
+        with open(chan_json) as cj:
+            chans = json.load(cj)
+
+        self.assertTrue(chan_json.is_file())
+        self.assertEqual(chans['25414'], 41)
+
+        if chan_json.is_file() is True:
+            shutil.rmtree(Path('window_maps'))
 
         
 
