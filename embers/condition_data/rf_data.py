564eb8b02f3c58f7cfe5e927a13aaf61b4b87c88
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-18 22:43:35 +1000

    Major cleanup of functions and added good docstrings for sphinx

--- embers/condition_data/rf_data.py
+++ embers/condition_data/rf_data.py
@@ -1,22 +1,44 @@
+"""
+=======
+RF Data
+=======
+A set of tools to decode raw rf data recored by
+RF Explorers and visualise waterfall plots.
+
+"""
+
+import re
 import time
 import numpy as np
-
-# import matplotlib
-# matplotlib.use("Agg")
-from embers.decode_rfdata.colormaps import spectral
+from pathlib import Path
 import matplotlib.pyplot as plt
+from embers.condition_data.colormaps import spectral
+
+spec, _ = spectral()
 
 
 def read_data(rf_file=None):
-    """Converts raw rf data from the RF Explorer
-    to a power and time array. The raw data is 
-    saved in binary format. Time is in UNIX format.
+    """
+    Converts raw rf binary data into a power array and
+    extracts a list of UNIX timestamps.
 
-    Args:
-        rf_file : Path to raw rf file
+    Parameters
+    ----------
+    :param rf_file: path to a rf data file
+    :type rf_file: str
 
-    Returns:
-        
+    Returns
+    -------
+    :returns:
+        - power - array of rf powers in dBm
+        - times - array of UNIX times
+
+    :rtype: (float, numpy.array(float))
+
+    Exeptions
+    ---------
+    :raises FileNotFoundError: input file does not exist
+
     """
 
     try:
@@ -35,20 +57,26 @@ def read_data(rf_file=None):
                 # The last two charachters are excluded - Newline char
                 data_lines.append(list(data[:-2]))
 
-            # I have no idea where the (-1/2) factor comes from
-            # Remenant of Jared Rasti's code. Seems to work
-            # Must be something to do with how data is written from RF
             # The (-1/2) converts an unsigned byte to a real value
             power = np.single(np.asarray(data_lines) * (-1 / 2))
             times = np.double(np.asarray(times))
 
             return (power, times)
-    except Exception:
-        print(f"Missing {filename}, moving on...")
+
+    except FileNotFoundError as e:
+        print(e)
+    except Exeption as e:
+        print(e)
 
 
 def tile_names():
-    """A list of tile reference and MWA tile names """
+    """A list of reference and MWA tile names 
+
+    Returns
+    -------
+    :return: list of tile names
+    :rtype: list[str]
+    """
 
     tiles = [
         "rf0XX",
@@ -84,76 +112,144 @@ def tile_names():
         "S36XX",
         "S36YY",
     ]
+
     return tiles
 
 
-def plot_waterfall(power, times, name, n_axes=True):
-    """Using power and times array, creates a waterfall
-    plot depicting satellite passes in the raw rf data.
-    By default this function will use n_axes = True to
-    convert unix times to human readable HH:MM local time
-    for the y-axis, and use the rf freqencies for the x-azis.
-    
-    If n_axes=False, will plot waterfall with time step
-    vs freq channel (0-112)
+def plt_waterfall(power, times):
+    """
+    Create waterfall `.pyplot.plot` object
+
+    waterfall created using parameters ``power``, ``times``
+    from :func:`~embers.condition_data.rf_data.read_data`. 
+    Default unix times are converted to a human readable
+    *HH:MM* format.
+
+    Parameters
+    ----------
+    :param power: `numpy.ndarray` object from :func:`~embers.condition_data.rf_data.read_data`
+    :param times: `numpy.ndarray` object from :func:`~embers.condition_data.rf_data.read_data`
+
+    Returns
+    -------
+        - plt - `.pyplot.plot` object
     """
 
+    # setting dynamic range of waterfall to be 30 dB above the median
     power_median = np.median(power)
     image = power - power_median
-    # setting dynamic range of waterfall to be 30 dB above the median
     vmin = 0
     vmax = 30
-
-    # Custom spectral colormap
-    cmap = spectral()
 
     plt.style.use("dark_background")
     fig = plt.figure(figsize=(7, 10))
     ax = fig.add_axes([0.12, 0.1, 0.72, 0.85])
-    im = ax.imshow(image, vmin=vmin, vmax=vmax, interpolation="none", cmap=cmap)
+    im = ax.imshow(image, vmin=vmin, vmax=vmax, interpolation="none", cmap=spec)
     cax = fig.add_axes([0.88, 0.1, 0.03, 0.85])
     fig.colorbar(im, cax=cax)
     ax.set_aspect("auto")
-    ax.set_title(f"Waterfall Plot: {name}")
 
-    if n_axes == True:
+    # Number of time steps on y-axis
+    number_t = 5
+    t_step = int(len(times) / (number_t - 1))
+    times = list(times)
+    times = times[::t_step]
 
-        # Number of time steps on y-axis
-        number_t = 5
-        t_step = int(len(times) / (number_t - 1))
-        times = list(times)
-        times = times[::t_step]
+    # Convert UNIX time to local HH:MM time
+    t_tz = []
+    for i in range(len(times)):
+        perth_t = float(times[i]) + 28800  # 28800=+8GMT @ PERTH
+        hms = time.strftime("%H:%M", time.gmtime(perth_t))
+        t_tz.append(hms)
 
-        t_tz = []
+    # Frequency: x-axis
+    start_freq = 137.15
+    stop_freq = 138.55
 
-        # Convert UNIX time to local HH:MM time
-        for i in range(len(times)):
+    # X-axis stuff
+    x_ax = image.shape[1]
+    freqs = np.arange(start_freq, stop_freq, 0.25)
+    x_ticks = np.arange(0, x_ax, (0.25 / 0.0125))  # .0125MHz/ch
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(freqs)
+    ax.set_xlabel("Freq [MHz]")
 
-            perth_t = float(times[i]) + 28800  # 28800=+8GMT @ PERTH
-            hms = time.strftime("%H:%M", time.gmtime(perth_t))
-            t_tz.append(hms)
-
-        # Frequency: x-axis
-        start_freq = 137.15
-        stop_freq = 138.55
-
-        # X-axis stuff
-        x_ax = image.shape[1]
-        freqs = np.arange(start_freq, stop_freq, 0.25)
-        x_ticks = np.arange(0, x_ax, (0.25 / 0.0125))  # .0125MHz/ch
-        ax.set_xticks(x_ticks)
-        ax.set_xticklabels(freqs)
-        ax.set_xlabel("Freq [MHz]")
-
-        # Y-axis stuff
-        y_ax = image.shape[0]
-        y_ticks = np.arange(0, y_ax, t_step)
-        ax.set_yticks(y_ticks)
-        ax.set_yticklabels(t_tz)
-        ax.set_ylabel("Time [HH:MM]")
-
-    else:
-        ax.set_xlabel("Freq Channel")
-        ax.set_ylabel("Time Step")
+    # Y-axis stuff
+    y_ax = image.shape[0]
+    y_ticks = np.arange(0, y_ax, t_step)
+    ax.set_yticks(y_ticks)
+    ax.set_yticklabels(t_tz)
+    ax.set_ylabel("Time [HH:MM]")
 
     return plt
+
+
+def single_waterfall(rf_file, out_dir):
+    """Saves a waterfall plot from rf data file
+    
+    Parameters
+    ----------
+    :param rf_file: path to a rf data file
+    :type rf_file: str
+    :param out_dir: path to output directory
+    :type out_dir: str
+
+    Returns
+    -------
+    :return: waterfall plot saved by `matplotlib.pyplot.savefig`
+
+    """
+
+    rf_name = Path(rf_file).stem
+
+    power, times = read_data(rf_file)
+    plt = plt_waterfall(power, times)
+    plt.title(f"Waterfall plot: {rf_name}")
+
+    # Make out_dir if it doesn't exist
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
+    plt.savefig(f"{out_dir}/{rf_name}.png")
+    plt.close()
+
+
+def batch_waterfall(tile, time_stamp, data_dir, out_dir):
+    """Saves a waterfall plot for a batch of rf data files
+    
+    Parameters
+    ----------
+    :param tile: tile name 
+    :type tile: str
+    :param time_stamp: when the rf observation began 
+    :type time_stamp: str [*YYYY-MM-DD-HH:MM*]
+    :param data_dir: path to root of data directory
+    :type data_dir: str
+    :param out_dir: path to output directory
+    :type out_dir: str
+
+    Returns
+    -------
+    :return: waterfall plot saved by `matplotlib.pyplot.savefig`
+
+    """
+
+    rf_name = f"{tile}_{time_stamp}"
+    date = re.search(r"\d{4}.\d{2}.\d{2}", time_stamp)[0]
+    rf_path = Path(f"{data_dir}/{tile}/{date}")
+
+    try:
+        power, times = read_data(f"{rf_path}/{rf_name}.txt")
+
+        plt = plt_waterfall(power, times)
+        plt.title(f"Waterfall plot: {rf_name}")
+
+        # Make out_dir if it doesn't exist
+        save_dir = Path(f"{out_dir}/waterfalls/{date}/{time_stamp}")
+        save_dir.mkdir(parents=True, exist_ok=True)
+
+        plt.savefig(f"{save_dir}/{rf_name}.png")
+        plt.close()
+
+        return f"Waterfall {rf_name}.png saved"
+
+    except Exception as e:
+        return e
