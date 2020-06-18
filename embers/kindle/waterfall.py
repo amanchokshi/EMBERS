821e039b088550410764129c526e8f217385fdc1
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-18 23:19:34 +1000

    Added main function to execute waterfall

--- embers/kindle/waterfall.py
+++ embers/kindle/waterfall.py
@@ -1,7 +1,14 @@
+"""
+==========
+Waterfalls
+==========
+Saves a single waterfall plot to ``./embers_out/condition_data``
+
+"""
+
 import argparse
+from pathlib import Path
 from embers.condition_data.rf_data import single_waterfall
-
-
 
 parser = argparse.ArgumentParser(
     description="""
@@ -21,6 +28,9 @@ args = parser.parse_args()
 rf_file = args.rf_file
 out_dir = args.out_dir
 
-single_waterfall(rf_file, out_dir)
-
-
+def main():
+    """Execute waterfall from terminal"""
+    
+    rf_name = Path(rf_file).stem
+    print(f"Waterfall plot saved to ./{out_dir}/{rf_name}.png")
+    single_waterfall(rf_file, out_dir)
