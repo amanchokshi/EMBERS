a872c0ebbb8a54149e022b19fae63d085a3c6e0c
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-20 03:48:28 +1000

    Fixed a bunch of hidden _vars

--- embers/kindle/colormaps.py
+++ embers/kindle/colormaps.py
@@ -20,24 +20,24 @@ _spec, _spec_r = spectral()
 _jade, _jade_r = jade()
 
 
-parser = argparse.ArgumentParser(
+_parser = argparse.ArgumentParser(
     description="""
     Visualization of custom ember colormaps
     """
 )
 
-parser.add_argument(
+_parser.add_argument(
     "--out_dir",
     metavar="\b",
     default="./embers_out/condition_data",
     help="Dir where colormap sample plot is saved. Default=./embers_out/condition_data",
 )
 
-args = parser.parse_args()
-out_dir = Path(args.out_dir)
+_args = _parser.parse_args()
+_out_dir = Path(_args.out_dir)
 
 # Make outdir if it doesn't exist
-out_dir.mkdir(parents=True, exist_ok=True)
+_out_dir.mkdir(parents=True, exist_ok=True)
 
 
 def waves_2d():
@@ -114,5 +114,5 @@ def plt_colormaps(spec, spec_r, jade, jade_r, out_dir):
 def main():
     """Execute colormaps from terminal."""
 
-    print(f"Plot of embers colormaps saved to ./{out_dir}/colormaps.png")
-    plt_colormaps(spec, spec_r, jade, jade_r, out_dir)
+    print(f"Plot of embers colormaps saved to ./{_out_dir}/colormaps.png")
+    plt_colormaps(_spec, _spec_r, _jade, _jade_r, _out_dir)
