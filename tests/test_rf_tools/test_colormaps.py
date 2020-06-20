08048c00669704c0b3a54ddb1ac3c3486be90d45
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-20 23:54:58 +1000

    19 tests passing. Need to figue out relative pasth

--- tests/test_rf_tools/test_colormaps.py
+++ tests/test_rf_tools/test_colormaps.py
@@ -1,19 +1,22 @@
 import pytest
 from embers.rf_tools.colormaps import spectral, jade
 
+
 def test_spectral():
     spec, _ = spectral()
     assert type(spec).__name__ == "ListedColormap"
+
 
 def test_spectral_r():
     _, spec_r = spectral()
     assert type(spec_r).__name__ == "ListedColormap"
 
+
 def test_jade():
     ja, _ = jade()
     assert type(ja).__name__ == "ListedColormap"
 
+
 def test_jade_r():
     _, ja_r = jade()
     assert type(ja_r).__name__ == "ListedColormap"
-
--- tests/test_rf_tools/test_rf_data.py
+++ tests/test_rf_tools/test_rf_data.py
@@ -1,55 +1,95 @@
 import pytest
-from embers.rf_tools import rf_data as rfd
-
-#power, times = rfd.read_data(rf_file='../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
-#print(type(times).__name__)
-print(len(rfd.tile_pairs(rfd.tile_names)))
+from embers.rf_tools.rf_data import (
+    read_data,
+    tile_names,
+    tile_pairs,
+    time_tree,
+    plt_waterfall,
+    single_waterfall,
+    batch_waterfall,
+)
 
 
 def test_read_data_power_shape():
-    power, _ = rfd.read_data(rf_file='../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
+    power, _ = read_data(
+        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
     assert power.shape == (16655, 112)
 
+
 def test_read_data_times_shape():
-    _, times = rfd.read_data(rf_file='../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
+    _, times = read_data(
+        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
     assert list(times.shape)[0] == 16655
 
+
 def test_read_data_power_times_shape():
-    power, times = rfd.read_data(rf_file='../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
+    power, times = read_data(
+        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
     assert list(times.shape)[0] == list(power.shape)[0]
 
+
 def test_read_data_return_type():
-    _, times = rfd.read_data(rf_file='../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
-    assert type(times).__name__ == 'ndarray'
+    _, times = read_data(
+        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
+    assert type(times).__name__ == "ndarray"
+
 
 def test_read_data_time_arrow():
-    _, times = rfd.read_data(rf_file='../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt')
+    _, times = read_data(
+        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
     assert times[0] <= times[-1]
 
+
 def test_tile_names_first():
-    tiles = rfd.tile_names()
-    assert tiles[0] == 'rf0XX'
+    tiles = tile_names()
+    assert tiles[0] == "rf0XX"
+
 
 def test_tile_names_last():
-    tiles = rfd.tile_names()
-    assert tiles[-1] == 'S36YY'
+    tiles = tile_names()
+    assert tiles[-1] == "S36YY"
+
 
 def test_tile_names_length():
-    tiles = rfd.tile_names()
+    tiles = tile_names()
     assert len(tiles) == 32
 
+
 def test_tile_pairs_first():
-    pairs = rfd.tile_pairs(rfd.tile_names)
-    assert pairs[0] == ('rf0XX', 'S06XX')
+    pairs = tile_pairs(tile_names)
+    assert pairs[0] == ("rf0XX", "S06XX")
+
 
 def test_tile_pairs_last():
-    pairs = rfd.tile_pairs(rfd.tile_names)
-    assert pairs[-1] == ('rf1YY', 'S36YY')
+    pairs = tile_pairs(tile_names)
+    assert pairs[-1] == ("rf1YY", "S36YY")
+
 
 def test_tile_pairs_length():
-    pairs = rfd.tile_pairs(rfd.tile_names)
+    pairs = tile_pairs(tile_names)
     assert len(pairs) == 56
 
 
+def test_time_tree_dates():
+    dates, _ = time_tree("2020-01-01", "2020-01-02")
+    assert dates == ["2020-01-01", "2020-01-02"]
 
 
+def test_time_tree_time_stamps_start():
+    _, time_stamps = time_tree("2020-01-01", "2020-01-02")
+    assert time_stamps[0][0] == "2020-01-01-00:00"
+
+
+def test_time_tree_time_stamps_end():
+    _, time_stamps = time_tree("2020-01-01", "2020-01-02")
+    assert time_stamps[-1][-1] == "2020-01-02-23:30"
+
+
+def test_time_tree_time_stamps_size_day():
+    _, time_stamps = time_tree("2020-01-01", "2020-01-02")
+    assert len(time_stamps[0]) == 48
