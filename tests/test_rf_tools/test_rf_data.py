765b70dbc4f750f6d6871e7eac42bd50fd854fb4
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-21 00:32:00 +1000

    18 tests passing in test_rf_data

--- tests/test_rf_tools/test_rf_data.py
+++ tests/test_rf_tools/test_rf_data.py
@@ -1,4 +1,13 @@
+# Built-in imports
+import os
+from os import path
+
+# Package imports
 import pytest
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+# Embers imports
 from embers.rf_tools.rf_data import (
     read_data,
     tile_names,
@@ -9,38 +18,44 @@ from embers.rf_tools.rf_data import (
     batch_waterfall,
 )
 
+# Save the path to this directory
+dirpath = path.dirname(__file__)
+
+# Obtain path to directory with colormaps
+test_data = path.abspath(path.join(dirpath, '../data'))
+
 
 def test_read_data_power_shape():
     power, _ = read_data(
-        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
     )
     assert power.shape == (16655, 112)
 
 
 def test_read_data_times_shape():
     _, times = read_data(
-        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
     )
     assert list(times.shape)[0] == 16655
 
 
 def test_read_data_power_times_shape():
     power, times = read_data(
-        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
     )
     assert list(times.shape)[0] == list(power.shape)[0]
 
 
 def test_read_data_return_type():
     _, times = read_data(
-        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
     )
     assert type(times).__name__ == "ndarray"
 
 
 def test_read_data_time_arrow():
     _, times = read_data(
-        rf_file="../data/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
     )
     assert times[0] <= times[-1]
 
@@ -93,3 +108,40 @@ def test_time_tree_time_stamps_end():
 def test_time_tree_time_stamps_size_day():
     _, time_stamps = time_tree("2020-01-01", "2020-01-02")
     assert len(time_stamps[0]) == 48
+
+def test_plt_waterfall():
+    power, times = read_data(
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
+    plt = plt_waterfall(power, times, 'S06XX-test')
+    assert type(plt).__name__ == 'module'
+
+def test_plt_waterfall_savefig():
+    power, times = read_data(
+        rf_file=f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt"
+    )
+    plt = plt_waterfall(power, times, 'S06XX-test')
+    plt.savefig(f"{test_data}/plt_waterfall_test.png")
+    test_file_path = Path(f"{test_data}/plt_waterfall_test.png")
+    assert test_file_path.is_file() is True
+    if test_file_path.is_file():
+        test_file_path.unlink()
+
+def test_single_waterfall():
+    single_waterfall(f"{test_data}/rf_data/S06XX/2019-10-01/S06XX_2019-10-01-14:30.txt", f"{test_data}")
+    single_waterfall_png = Path(f"{test_data}/S06XX_2019-10-01-14:30.png")
+    assert single_waterfall_png.is_file() is True
+    if single_waterfall_png.is_file() is True:
+        single_waterfall_png.unlink()
+
+#def test_batch_waterfall():
+#    batch_waterfall('S06XX', '2019-10-01-14:30', f"{test_data}/rf_data", f"{test_data}")
+#    #single_waterfall_png = Path(f"{test_data}/single_waterfall.png")
+#    #assert single_waterfall_png.is_file is True
+#    #if single_waterfall_png.is_file is True:
+#    #    single_waterfall_png.unlink()
+
+
+
+
+
