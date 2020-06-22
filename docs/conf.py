32ec296a500f73cf822527f23cdabaea56282b1b
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-22 16:52:28 +1000

    Beam favicon

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ autodoc_member_order = 'bysource'
 # a list of builtin themes.
 #
 html_theme = 'default'
-
+html_favicon = '_static/favicon.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
