445c9e34a99c58837c13b5d67da48d976bd70ef3
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-22 18:02:23 +1000

    Beam favicon

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,8 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The master toctree document.
 master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
@@ -66,12 +68,15 @@ autodoc_member_order = 'bysource'
 # a list of builtin themes.
 #
 html_theme = 'default'
-html_favicon = '_static/favicon.png'
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = './_static/favicon.svg'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 
 # -- Extension configuration -------------------------------------------------
 
