81951a148a9ab60faab422757e12657858403564
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-27 01:56:03 +1000

    Converted to executable script. Add to setup.py now

--- embers/kindle/ephem_single.py
+++ embers/kindle/ephem_single.py
@@ -1,10 +1,11 @@
 import json
 import argparse
 import numpy as np
+import pkg_resources
 from pathlib import Path
 from embers.sat_utils.sat_ephemeris import save_ephem
 
-parser = argparse.ArgumentParser(
+_parser = argparse.ArgumentParser(
     description="""
         Code which converts the TLE files downloaded with download_TLE.py
         into satellite ephemeris data: rise time, set time, alt/az arrays
@@ -13,37 +14,63 @@ parser = argparse.ArgumentParser(
         """
 )
 
-parser.add_argument(
+_parser.add_argument(
     "--sat", metavar="\b", help="The Norad cat ID of satellite. Example: 21576"
 )
-parser.add_argument(
+_parser.add_argument(
     "--tle_dir",
     metavar="\b",
-    default="../../embers_out/sat_utils/TLE/",
-    help="Directory where TLE files are saved. Default=../../embers_out/sat_utils/TLE/",
+    type=json.loads,
+    default=(-26.703319, 116.670815, 337.83),
+    help="Geographic location where satellite ephemeris is to be determined. Default=MWA:(-26.703319, 116.670815, 337.83)",
 )
-parser.add_argument(
+_parser.add_argument(
     "--cadence",
     metavar="\b",
     type=int,
     default=4,
-    help="Rate at which sat alt/az is computed. Default=4s",
+    help="rate at which sat alt/az is computed. default=4s",
 )
-parser.add_argument(
+_parser.add_argument(
+    "--location",
+    metavar="\b",
+    default=4,
+    help="rate at which sat alt/az is computed. default=4s",
+)
+_parser.add_argument(
     "--out_dir",
     metavar="\b",
     default="./embers_out/sat_utils/",
     help="Path to output directory. Default=./embers_out/sat_utils/",
 )
 
-args = parser.parse_args()
-sat_name = args.sat
-tle_dir = args.tle_dir
-cadence = args.cadence
-out_dir = args.out_dir
+_args = _parser.parse_args()
+_sat_name = _args.sat
+_tle_dir = _args.tle_dir
+_cadence = _args.cadence
+_location = _args.location
+_out_dir = _args.out_dir
 
-# Position of MWA site in Lat/Lon/Elevation
-MWA = (-26.703319, 116.670815, 337.83)
+# if no input file provided, use sample package data
+if _tle_dir == "":
+    print("----------------------------------------------------")
+    print("No tle_dir path provided, using packaged sample data")
+    print(">>> ephem_single --help, for more options")
+    print("----------------------------------------------------")
+    _tle_file = Path(
+        pkg_resources.resource_filename("embers.kindle", "data/TLE/25984.txt")
+    )
+    _sat_name = _tle_file.stem
+    _tle_dir = _tle_file.parents[0]
 
-save_ephem(sat_name, tle_dir=tle_dir, cadence=cadence, location=MWA, out_dir=out_dir)
 
+def main():
+    """Execute save ephem from terminal."""
+
+    save_ephem(
+        _sat_name,
+        tle_dir=_tle_dir,
+        cadence=_cadence,
+        location=_location,
+        out_dir=_out_dir,
+    )
