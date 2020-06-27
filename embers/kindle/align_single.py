223bf6560b774aa8b5b35a257c7c22b9c0ae8adb
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-27 20:11:37 +1000

    Moved plot_savgol_interp to align_data

--- embers/kindle/align_single.py
+++ embers/kindle/align_single.py
@@ -3,7 +3,7 @@ Align Single
 ============
 
 Plot a single frequency channel of raw rf data and the result of
-interpolation and Savitzky–Golay smoothing. Save plot to ``./embers_out/rf_tools``
+interpolation and Savitzky–Golay smoothing. Save plot to :samp:`./embers_out/rf_tools`
 
 """
 
@@ -11,7 +11,7 @@ import argparse
 import pkg_resources
 from pathlib import Path
 import matplotlib.pyplot as plt
-from embers.rf_tools.align_data import savgol_interp
+from embers.rf_tools.align_data import plot_savgol_interp
 
 _parser = argparse.ArgumentParser(
     description="""
@@ -22,9 +22,7 @@ _parser = argparse.ArgumentParser(
 _parser.add_argument(
     "--ref_file", metavar="\b", default="", help="Reference rf data file"
 )
-
 _parser.add_argument("--tile_file", metavar="\b", default="", help="Tile rf data file")
-
 _parser.add_argument(
     "--savgol_window_1",
     metavar="\b",
@@ -32,7 +30,6 @@ _parser.add_argument(
     default=11,
     help="First savgol window. Default=11",
 )
-
 _parser.add_argument(
     "--savgol_window_2",
     metavar="\b",
@@ -40,18 +37,15 @@ _parser.add_argument(
     default=15,
     help="Second savgol window. Default=15",
 )
-
 _parser.add_argument(
     "--polyorder", metavar="\b", type=int, default=2, help="Polynomial order. Default=2"
 )
-
 _parser.add_argument(
     "--interp_type",
     metavar="\b",
     default="cubic",
     help="Interpolation type. Default=cubic",
 )
-
 _parser.add_argument(
     "--interp_freq",
     metavar="\b",
@@ -59,11 +53,9 @@ _parser.add_argument(
     default=1,
     help="Interpolation frequency. Default=1",
 )
-
 _parser.add_argument(
     "--channel", metavar="\b", default="", help="Frequency channel to plot"
 )
-
 _parser.add_argument(
     "--out_dir",
     metavar="\b",
@@ -103,90 +95,6 @@ if _channel == "":
     print(">>> savgol_interp_sample --help, for more options")
     print("----------------------------------------------------------")
     _channel = 59
-
-
-def plot_savgol_interp(
-    ref=None,
-    tile=None,
-    savgol_window_1=None,
-    savgol_window_2=None,
-    polyorder=None,
-    interp_type=None,
-    interp_freq=None,
-    channel=None,
-    out_dir=None,
-):
-
-    (
-        ref_ali,
-        tile_ali,
-        time_array,
-        ref_power,
-        tile_power,
-        ref_time,
-        tile_time,
-    ) = savgol_interp(
-        ref=ref,
-        tile=tile,
-        savgol_window_1=savgol_window_1,
-        savgol_window_2=savgol_window_2,
-        polyorder=polyorder,
-        interp_type=interp_type,
-        interp_freq=interp_freq,
-    )
-
-    # Sample align plot
-    plt.style.use("seaborn")
-    plt.rcParams["figure.figsize"] = (9, 6)
-
-    # convert times to minuts from first datapoint
-    time_array = (time_array - time_array[0]) / 60
-    ref_time = (ref_time - ref_time[0]) / 60
-    tile_time = (tile_time - tile_time[0]) / 60
-
-    plt.plot(
-        time_array,
-        tile_ali[::, channel],
-        color="#e23a4e",
-        alpha=0.9,
-        label="tile savgol",
-    )
-    plt.scatter(
-        tile_time,
-        tile_power[::, channel],
-        color="#f78b51",
-        marker=".",
-        alpha=0.6,
-        label="tile raw",
-    )
-
-    plt.plot(
-        time_array,
-        ref_ali[::, channel],
-        color="#252b40",
-        alpha=0.9,
-        label="ref savgol",
-    )
-    plt.scatter(
-        ref_time,
-        ref_power[::, channel],
-        color="#6a82bb",
-        marker=".",
-        alpha=0.6,
-        label="ref raw",
-    )
-
-    leg = plt.legend(loc="upper left", frameon=True)
-    leg.get_frame().set_facecolor("white")
-    for l in leg.legendHandles:
-        l.set_alpha(1)
-
-    plt.ylim(-110, -20)
-    plt.ylabel("Raw Power [dBm]")
-    plt.xlabel("Time [min]")
-    plt.tight_layout()
-    Path(f"{out_dir}").mkdir(parents=True, exist_ok=True)
-    plt.savefig(f"{out_dir}/savgol_interp_sample.png")
 
 
 def main():
