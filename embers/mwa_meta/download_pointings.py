80679801b3a62ca599bbaebf5f28c34e7a6929a0
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-06-29 00:56:13 +1000

    Download_pointings script cleaned up and pretty json

--- embers/mwa_meta/download_pointings.py
+++ embers/mwa_meta/download_pointings.py
@@ -2,14 +2,14 @@ import json, wget, time
 from pathlib import Path
 from astropy.time import Time
 
+
 def wget_meta(start, stop, num_pages, out_dir):
     start_gps = int(Time(start, format="isot").gps)
     stop_gps = int(Time(stop, format="isot").gps)
     for npg in range(num_pages):
-        time.sleep(10)
-        cerberus_url = f"http://ws.mwatelescope.org/metadata/find?mintime={start_gps}&maxtime={stop_gps}&extended=1&page={npg+1}"
-        print(cerberus_url)
-        print(f"Downloading page {npg+1} of metadata")
+        time.sleep(28)
+        cerberus_url = f"http://ws.mwatelescope.org/metadata/find?mintime={start_gps}&maxtime={stop_gps}&extended=1&page={npg+1}&pretty=1"
+        print(f"\nDownloading page {npg+1} of metadata")
         wget.download(cerberus_url, f"{out_dir}/pointing_{npg+1:03d}.json")
 
 
@@ -40,9 +40,7 @@ if __name__ == "__main__":
         help="Total number of pages between start and stop, determined from http://ws.mwatelescope.org/metadata/find",
     )
     parser.add_argument(
-        "--out_dir",
-        metavar="\b",
-        help="Output directory for json metadata",
+        "--out_dir", metavar="\b", help="Output directory for json metadata",
     )
 
     args = parser.parse_args()
@@ -52,5 +50,5 @@ if __name__ == "__main__":
     out_dir = Path(args.out_dir)
 
     out_dir.mkdir(parents=True, exist_ok=True)
-    
+
     wget_meta(start, stop, num_pgs, out_dir)
