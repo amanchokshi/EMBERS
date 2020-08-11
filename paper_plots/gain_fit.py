efde8841e33d81e6d9dd5dea19b5d563de081813
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-08-12 01:28:13 +1000

    RFE Gain calibration

--- paper_plots/gain_fit.py
+++ paper_plots/gain_fit.py
@@ -1,40 +1,40 @@
-#import sys
-#import json
-#import argparse
-#import matplotlib
-#import numpy as np
+# import sys
+# import json
+# import argparse
+# import matplotlib
+# import numpy as np
 #
-#matplotlib.use("Agg")
-#from pathlib import Path
-#import scipy.optimize as opt
-#import matplotlib.pyplot as plt
-#import matplotlib.gridspec as gs
-#from scipy.stats import median_absolute_deviation as mad
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
-#import numpy.polynomial.polynomial as poly
-#from scipy.stats import binned_statistic
-#import scipy.optimize as opt
+# matplotlib.use("Agg")
+# from pathlib import Path
+# import scipy.optimize as opt
+# import matplotlib.pyplot as plt
+# import matplotlib.gridspec as gs
+# from scipy.stats import median_absolute_deviation as mad
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
+# import numpy.polynomial.polynomial as poly
+# from scipy.stats import binned_statistic
+# import scipy.optimize as opt
 #
-#sys.path.append("../decode_rf_data")
-#from colormap import spectral
+# sys.path.append("../decode_rf_data")
+# from colormap import spectral
 #
-#cmap = spectral()
+# cmap = spectral()
 #
-#gain_files = [
+# gain_files = [
 #    item for item in Path("../../outputs/paper_plots/gain_fit/").glob("*.json")
-#]
-#names = [i.name.split(".")[0] for i in gain_files]
+# ]
+# names = [i.name.split(".")[0] for i in gain_files]
 #
-#pass_data = []
-#pass_resi = []
+# pass_data = []
+# pass_resi = []
 #
-#for n, f in enumerate(gain_files):
+# for n, f in enumerate(gain_files):
 #    with open(f, "r") as data:
 #        rfe = json.load(data)
 #        pass_data.extend(rfe["pass_data"])
 #        pass_resi.extend(rfe["pass_resi"])
 #
-#nice_fonts = {
+# nice_fonts = {
 #    # Use LaTeX to write all text
 #    # "text.usetex": True,
 #    "font.family": "sans-serif",
@@ -45,42 +45,42 @@
 #    "legend.fontsize": 6,
 #    "xtick.labelsize": 8,
 #    "ytick.labelsize": 8,
-#}
+# }
 #
-#plt.rcParams.update(nice_fonts)
+# plt.rcParams.update(nice_fonts)
 #
-#fig = plt.figure(figsize=(3.6, 2.4))
+# fig = plt.figure(figsize=(3.6, 2.4))
 ## fig = plt.figure()
 #
-#plt.hexbin(pass_data, pass_resi, gridsize=77, cmap=cmap, alpha=0.99, zorder=0)
+# plt.hexbin(pass_data, pass_resi, gridsize=77, cmap=cmap, alpha=0.99, zorder=0)
 #
-#pass_data = np.array(pass_data)
-#pass_resi = np.array(pass_resi)
-#filtr = np.where(np.logical_and(pass_data <= -20, pass_data >= -70))
-#pass_data = pass_data[filtr]
-#pass_resi = pass_resi[filtr]
+# pass_data = np.array(pass_data)
+# pass_resi = np.array(pass_resi)
+# filtr = np.where(np.logical_and(pass_data <= -20, pass_data >= -70))
+# pass_data = pass_data[filtr]
+# pass_resi = pass_resi[filtr]
 #
 ## Median of binned data
-#bin_med, bin_edges, binnumber = binned_statistic(
+# bin_med, bin_edges, binnumber = binned_statistic(
 #    pass_data, pass_resi, statistic="median", bins=20
-#)
-#bin_width = bin_edges[1] - bin_edges[0]
-#bin_centers = bin_edges[1:] - bin_width / 2
+# )
+# bin_width = bin_edges[1] - bin_edges[0]
+# bin_centers = bin_edges[1:] - bin_width / 2
 #
 ## Now look at data between -50, -30, where gains vary
-#filtr = np.where(np.logical_and(pass_data <= -28, pass_data >= -52))
-#pass_data = pass_data[filtr]
-#pass_resi = pass_resi[filtr]
+# filtr = np.where(np.logical_and(pass_data <= -28, pass_data >= -52))
+# pass_data = pass_data[filtr]
+# pass_resi = pass_resi[filtr]
 #
-#z = np.polyfit(pass_data, pass_resi, 1)
-#f = np.poly1d(z)
+# z = np.polyfit(pass_data, pass_resi, 1)
+# f = np.poly1d(z)
 #
-#x_f = [f.roots[0], -45, -40, -35, -30, -25, -20]
-#y_f = f(x_f)
+# x_f = [f.roots[0], -45, -40, -35, -30, -25, -20]
+# y_f = f(x_f)
 #
 ## plt.plot(x_f, y_f, color='w', lw=2.1, marker='X', markeredgecolor='k', markeredgewidth=0.7, markersize=2.1, alpha=1, label='Gain fit', zorder=1)
-#plt.plot(x_f, y_f, color="w", lw=1.8, alpha=1, label="Gain fit", zorder=1)
-#plt.scatter(
+# plt.plot(x_f, y_f, color="w", lw=1.8, alpha=1, label="Gain fit", zorder=1)
+# plt.scatter(
 #    bin_centers,
 #    bin_med,
 #    marker="h",
@@ -91,23 +91,198 @@
 #    alpha=1,
 #    label="Median residuals",
 #    zorder=2,
-#)
-#plt.scatter(f.roots[0], 0, marker="o", s=9, facecolors="w", alpha=1)
+# )
+# plt.scatter(f.roots[0], 0, marker="o", s=9, facecolors="w", alpha=1)
 #
-#leg = plt.legend(
+# leg = plt.legend(
 #    loc="lower right", frameon=True, framealpha=0.7, markerscale=0.9, handlelength=1
-#)
-#leg.get_frame().set_facecolor("#cccccc")
-#for l in leg.legendHandles:
+# )
+# leg.get_frame().set_facecolor("#cccccc")
+# for l in leg.legendHandles:
 #    l.set_alpha(1)
 #
 #
-#plt.xlabel("Observed power [dBm]")
-#plt.ylabel("Residuals power [dB]")
-#plt.xlim([-70, -20])
-#plt.ylim([-10, 15])
-#plt.tick_params(axis="both", length=0)
-#plt.grid(color="#cccccc", alpha=0.21, lw=1)
-#plt.box(None)
-#plt.tight_layout()
-#plt.savefig(f"../../outputs/paper_plots/rfe_gain_fit.pdf", bbox_inches="tight")
+# plt.xlabel("Observed power [dBm]")
+# plt.ylabel("Residuals power [dB]")
+# plt.xlim([-70, -20])
+# plt.ylim([-10, 15])
+# plt.tick_params(axis="both", length=0)
+# plt.grid(color="#cccccc", alpha=0.21, lw=1)
+# plt.box(None)
+# plt.tight_layout()
+# plt.savefig(f"../../outputs/paper_plots/rfe_gain_fit.pdf", bbox_inches="tight")
+
+import argparse
+import json
+from pathlib import Path
+
+import numpy as np
+from embers.rf_tools.colormaps import spectral
+from matplotlib import pyplot as plt
+from scipy.stats import binned_statistic
+
+spec, _ = spectral()
+
+parser = argparse.ArgumentParser(
+    description="""
+    Calibrate non-linear gain variations of RF Explorers at high power
+    """
+)
+
+parser.add_argument(
+    "--start_gain",
+    metavar="\b",
+    default=-50,
+    type=int,
+    help="Power at which RFE gain variations begin. Default: -50dBm",
+)
+
+parser.add_argument(
+    "--stop_gain",
+    metavar="\b",
+    default=-30,
+    type=int,
+    help="Power at which RFE gain variations saturate. Default: -30dBm",
+)
+
+parser.add_argument(
+    "--rfe_cali",
+    metavar="\b",
+    default="../embers_out/tile_maps/rfe_calibration",
+    help="Directory where RF Explorer calibration data lives. Default=../embers_out/tile_maps/rfe_calibration",
+)
+
+parser.add_argument(
+    "--out_dir",
+    metavar="\b",
+    default="../embers_out/paper_plots",
+    help="Output directory. Default=../embers_out/paper_plots",
+)
+
+args = parser.parse_args()
+start_gain = args.start_gain
+stop_gain = args.stop_gain
+rfe_cali = args.rfe_cali
+out_dir = args.out_dir
+
+
+def rfe_collate_cali(start_gain, stop_gain, rfe_cali_dir):
+    """Collate RF Explorer gain calibration data from all MWA tile pairs, and plot a gain solution.
+
+    :param start_gain: Power at which RFE gain variations begin. Ex: -50dBm
+    :param stop_gain: Power at which RFE gain variations saturate. Ex: -30dBm
+    :param rfe_cali_dir: Path to directory which contains gain calibration data saved by :func:`~embers.tile_maps.tile_maps.rfe_calibration`
+
+    :returns:
+        - Plot of global gain calibration solution and polynomial fit saved to :samp:`.npz` in the out_dir
+
+    """
+
+    # find all rfe_gain json files
+    gain_files = [item for item in Path(rfe_cali_dir).glob("*.json")]
+
+    # Combine data from all RF Explorers
+    pass_data = []
+    pass_resi = []
+
+    for n, f in enumerate(gain_files):
+        with open(f, "r") as data:
+            rfe = json.load(data)
+            pass_data.extend(rfe["pass_data"])
+            pass_resi.extend(rfe["pass_resi"])
+
+    nice_fonts = {
+        # Use LaTeX to write all text
+        # "text.usetex": True,
+        "font.family": "sans-serif",
+        # Use 10pt font in plots, to match 10pt font in document
+        "axes.labelsize": 10,
+        "font.size": 10,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": 6,
+        "xtick.labelsize": 8,
+        "ytick.labelsize": 8,
+    }
+
+    plt.rcParams.update(nice_fonts)
+
+    plt.figure(figsize=(3.6, 2.4))
+
+    plt.hexbin(pass_data, pass_resi, gridsize=121, cmap=spec, alpha=0.99, zorder=0)
+
+    pass_data = np.array(pass_data)
+    pass_resi = np.array(pass_resi)
+
+    # Clean up huge noisy outliers
+    filtr = np.where(np.logical_and(pass_data <= -25, pass_data >= -65))
+    pass_data = pass_data[filtr]
+    pass_resi = pass_resi[filtr]
+
+    # Median of binned data
+    bin_med, bin_edges, binnumber = binned_statistic(
+        pass_data, pass_resi, statistic="median", bins=16
+    )
+    bin_width = bin_edges[1] - bin_edges[0]
+    bin_centers = bin_edges[1:] - bin_width / 2
+
+    # Now look at data between -50, -30, where gains vary
+    filtr = np.where(np.logical_and(pass_data >= start_gain, pass_data <= stop_gain))
+    pass_data = pass_data[filtr]
+    pass_resi = pass_resi[filtr]
+
+    # Linear fit to RFE data between -50, -30 dBm
+    poly = np.polyfit(pass_data, pass_resi, 2)
+
+    # Mathematical function of fit, which can be evaluated anywhere
+    f = np.poly1d(poly)
+
+    # save polyfit to file
+    np.save(f"{rfe_cali_dir}/rfe_gain_fit.npy", poly)
+
+    # x_f = [f.roots[0], -45, -40, -35, -30, -25, -20]
+    x_f = np.linspace(max(f.roots), -25, num=10)
+    y_f = f(x_f)
+
+    plt.plot(
+        x_f,
+        y_f,
+        color="w",
+        lw=2.1,
+        marker="s",
+        markeredgecolor="k",
+        markeredgewidth=1.6,
+        markersize=4.9,
+        alpha=1,
+        label="Gain fit",
+        zorder=1,
+    )
+    plt.scatter(
+        bin_centers,
+        bin_med,
+        marker="X",
+        s=36,
+        facecolors="#ee4540",
+        lw=0.9,
+        edgecolors="w",
+        alpha=1,
+        label="Median residuals",
+        zorder=2,
+    )
+
+    leg = plt.legend(loc="lower right", frameon=True, markerscale=0.9, handlelength=1.4)
+    leg.get_frame().set_facecolor("#cccccc")
+    for le in leg.legendHandles:
+        le.set_alpha(0.77)
+
+    plt.xlabel("Observed power [dBm]")
+    plt.ylabel("Residuals power [dB]")
+    plt.xlim([-65, -25])
+    plt.ylim([-10, 15])
+    plt.tick_params(axis="both", length=0)
+    plt.grid(color="#cccccc", alpha=0.36, lw=1.2)
+    plt.box(None)
+    plt.tight_layout()
+    plt.savefig(f"{out_dir}/rfe_gain_fit.pdf", bbox_inches="tight")
+
+
+rfe_collate_cali(start_gain, stop_gain, rfe_cali)
