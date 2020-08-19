49115220855261b9d0be46bfb3b0a39eac52d6ab
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-08-20 02:04:37 +1000

    Added ephem data npz file and working on testing chrono_ephem

--- tests/test_sat_utils/test_chrono_ephem.py
+++ tests/test_sat_utils/test_chrono_ephem.py
@@ -1,7 +1,9 @@
+import json
 import shutil
 from os import path
 from pathlib import Path
 
+import numpy as np
 from embers.sat_utils.chrono_ephem import (interp_ephem, obs_times,
                                            save_chrono_ephem, write_json)
 
@@ -10,6 +12,17 @@ dirpath = path.dirname(__file__)
 
 # Obtain path to directory with test_data
 test_data = path.abspath(path.join(dirpath, "../data"))
+
+ephem_data = Path(f"{test_data}/sat_utils/ephem_data/25986.npz")
+
+sat_ephem = np.load(ephem_data, allow_pickle=True)
+t_array = sat_ephem["time_array"]
+s_alt = sat_ephem["sat_alt"]
+s_az = sat_ephem["sat_az"]
+#  s_id = str(sat_ephem["sat_id"])
+
+time_interp, sat_alt, sat_az = interp_ephem(t_array[0], s_alt[0], s_az[0], "cubic", 1)
+print(len(time_interp))
 
 
 def test_obs_times_human():
@@ -25,4 +38,44 @@ def test_obs_times_interval():
     )
     assert obs_unix_end[0] - obs_unix[0] == 1800
 
-#  def test_interp_ephem():
+
+def test_interp_ephem_shape():
+    ephem_data = Path(f"{test_data}/sat_utils/ephem_data/25986.npz")
+
+    sat_ephem = np.load(ephem_data, allow_pickle=True)
+    t_array = sat_ephem["time_array"]
+    s_alt = sat_ephem["sat_alt"]
+    s_az = sat_ephem["sat_az"]
+    #  s_id = str(sat_ephem["sat_id"])
+
+    time_interp, sat_alt, sat_az = interp_ephem(
+        t_array[0], s_alt[0], s_az[0], "cubic", 1
+    )
+    assert len(time_interp) == 931
+
+
+def test_interp_ephem_sky():
+    ephem_data = Path(f"{test_data}/sat_utils/ephem_data/25986.npz")
+
+    sat_ephem = np.load(ephem_data, allow_pickle=True)
+    t_array = sat_ephem["time_array"]
+    s_alt = sat_ephem["sat_alt"]
+    s_az = sat_ephem["sat_az"]
+    #  s_id = str(sat_ephem["sat_id"])
+
+    time_interp, sat_alt, sat_az = interp_ephem(
+        t_array[0], s_alt[0], s_az[0], "cubic", 1
+    )
+    assert len(sat_alt) == len(sat_az)
+
+
+def test_write_json():
+    data = [1, 2, 3]
+    write_json(data, "tmp.json", f"{test_data}/sat_utils")
+    tmp_path = Path(f"{test_data}/sat_utils/tmp.json")
+    if tmp_path.is_file():
+        with open(tmp_path, "r") as f:
+            d_read = json.load(f)
+
+            assert d_read == data
+            tmp_path.unlink()
