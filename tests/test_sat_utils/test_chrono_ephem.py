d7bb4accdd22e3572e34e4a99c81466c6e2c5c78
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-08-20 02:15:59 +1000

    Chrono_ephem has achieved 100% coverage. hoooray

--- tests/test_sat_utils/test_chrono_ephem.py
+++ tests/test_sat_utils/test_chrono_ephem.py
@@ -13,17 +13,6 @@ dirpath = path.dirname(__file__)
 # Obtain path to directory with test_data
 test_data = path.abspath(path.join(dirpath, "../data"))
 
-ephem_data = Path(f"{test_data}/sat_utils/ephem_data/25986.npz")
-
-sat_ephem = np.load(ephem_data, allow_pickle=True)
-t_array = sat_ephem["time_array"]
-s_alt = sat_ephem["sat_alt"]
-s_az = sat_ephem["sat_az"]
-#  s_id = str(sat_ephem["sat_id"])
-
-time_interp, sat_alt, sat_az = interp_ephem(t_array[0], s_alt[0], s_az[0], "cubic", 1)
-print(len(time_interp))
-
 
 def test_obs_times_human():
     obs_time, obs_unix, obs_unix_end = obs_times(
@@ -79,3 +68,20 @@ def test_write_json():
 
             assert d_read == data
             tmp_path.unlink()
+
+
+def test_save_chrono_ephem():
+    out_dir = Path(f"{test_data}/sat_utils/ephem_chrono_tmp")
+    save_chrono_ephem(
+        "Australia/Perth",
+        "2019-10-01",
+        "2019-10-01",
+        "cubic",
+        1,
+        f"{test_data}/sat_utils/ephem_data",
+        out_dir,
+    )
+    files = [f.stem for f in out_dir.glob("*.json")]
+
+    assert len(files) == 48
+    shutil.rmtree(out_dir)
