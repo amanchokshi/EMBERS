fab6e345b6fc38911e6ca688aa7b6b1b69fa0e27
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-10-14 00:13:46 +1100

    Combined two rot_res plots. It's a beauty

--- paper_plots/cardinal_errors.py
+++ paper_plots/cardinal_errors.py
@@ -7,17 +7,21 @@
 import argparse
 from pathlib import Path
 
+import healpy as hp
 import matplotlib
 import numpy as np
 from astropy.stats import mad_std as mad
-from embers.rf_tools.colormaps import spectral
+from embers.rf_tools.colormaps import jade, spectral
 from embers.tile_maps.beam_utils import (chisq_fit_gain,
                                          healpix_cardinal_slices, map_slices,
                                          poly_fit, rotate_map)
+from healpy.rotator import euler_matrix_new as euler
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 
 matplotlib.use("Agg")
 _spec, _ = spectral()
+
 
 parser = argparse.ArgumentParser(
     description="""
@@ -121,8 +125,8 @@ def beam_slices(map_file, fee_map, nside):
 
         # slice the tile and fee maps along NS, EW.
         # the above gain factor is applied to full beam slices
-        NS_fee, EW_fee = healpix_cardinal_slices(nside, fee_r, 90)
-        NS_tile, EW_tile = map_slices(nside, tile_r, 90)
+        NS_fee, EW_fee = healpix_cardinal_slices(nside, fee_r, 85)
+        NS_tile, EW_tile = map_slices(nside, tile_r, 85)
 
         # Scale the data so that it best fits the beam slice
         NS_tile_med = NS_tile[0] - gain_NS[0]
@@ -144,6 +148,24 @@ def beam_slices(map_file, fee_map, nside):
         )
 
     return maps
+
+
+def reproject_map(nside, phi, healpix_array=None):
+    """Reproject beam map to be centered around healpix pixel instead of zenith.
+
+    :param nside: Healpix nside
+    :param phi: Angle to rotate beam by, from E --> N (anticlockwise)
+    :param healpix_array: Input healpix array to be rotated
+    :returns:
+        - Reprojected healpix map
+    """
+
+    vec = hp.pix2vec(nside, np.arange(hp.nside2npix(nside)))
+    eu_mat = euler(-phi, 0, 0, deg=True)
+    rot_map = hp.rotator.rotateVector(eu_mat, vec)
+    new_hp_inds = hp.vec2pix(nside, rot_map[0], rot_map[1], rot_map[2])
+
+    return healpix_array[new_hp_inds]
 
 
 def slice_residual():
@@ -216,21 +238,21 @@ def slice_residual():
     EW_YY_fit = poly_fit(za, EW_YY_res, EW_YY_res, 2)
 
     # Plotting stuff
-    plt.style.use("seaborn")
+    #  plt.style.use("seaborn")
 
-    nice_fonts = {
-        "font.family": "sans-serif",
-        "axes.labelsize": 8,
-        "axes.titlesize": 9,
-        "font.size": 8,
-        "legend.fontsize": 6,
-        "xtick.labelsize": 8,
-        "ytick.labelsize": 8,
-    }
+    #  nice_fonts = {
+    #  "font.family": "sans-serif",
+    #  "axes.labelsize": 8,
+    #  "axes.titlesize": 9,
+    #  "font.size": 8,
+    #  "legend.fontsize": 6,
+    #  "xtick.labelsize": 8,
+    #  "ytick.labelsize": 8,
+    #  }
 
-    plt.rcParams.update(nice_fonts)
+    #  plt.rcParams.update(nice_fonts)
 
-    plt.figure(figsize=(3.6, 2.4))
+    #  plt.figure(figsize=(3.6, 2.4))
 
     #  colors = _spec(np.linspace(0.17, 0.9, 4))
     colors = _spec([0.14, 0.77, 0.66, 0.35])
@@ -333,10 +355,180 @@ def slice_residual():
         le.set_alpha(1)
 
     plt.ylim([-4, 4])
+    #  plt.xlabel("Zenith Angle [degrees]")
+    plt.ylabel("Residual Power [dB]")
+    #  plt.tight_layout()
+    #  plt.savefig("ref_rot/rot_resi.pdf")
+    return plt
+
+
+#########################################
+
+if __name__ == "__main__":
+
+    spec, _ = spectral()
+    jade, _ = jade()
+    #  nside = 512
+
+    ref_model = "ref_rot/ref_dipole_models.npz"
+
+    # Load reference FEE model
+    # Rotate the fee models by -pi/2 to move model from spherical (E=0) to Alt/Az (N=0)
+    ref_fee_model = np.load(ref_model, allow_pickle=True)
+
+    ref_xx = rotated_fee = rotate_map(
+        512, angle=-(1 * np.pi) / 2.0, healpix_array=ref_fee_model["XX"]
+    )
+    ref_yy = rotated_fee = rotate_map(
+        512, angle=-(1 * np.pi) / 2.0, healpix_array=ref_fee_model["YY"]
+    )
+
+    # Plotting stuff
+    plt.style.use("seaborn")
+
+    nice_fonts = {
+        "font.family": "sans-serif",
+        "axes.labelsize": 8,
+        "axes.titlesize": 9,
+        "font.size": 8,
+        "legend.fontsize": 5,
+        "xtick.labelsize": 8,
+        "ytick.labelsize": 8,
+    }
+
+    plt.rcParams.update(nice_fonts)
+
+    fig = plt.figure(figsize=(3.6, 4.9))
+
+    ax1 = fig.add_subplot(2, 1, 1)
+    car_err = slice_residual()
+    ax1.set_xticklabels([])
+    ax1.set_yticks([-4, -2, 0, 2, 4])
+    ax1.set_xlim([-96, 96])
+    ax1.set_ylim([-3, 4])
+    ax1.text(0.004, 0.93, r"($i$)", horizontalalignment="left", transform=ax1.transAxes)
+
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.set_yticks([-2, -1, 0, 1, 2])
+    ax2.set_xlim([-96, 96])
+    ax2.text(0.004, 0.93, r"($ii$)", horizontalalignment="left", transform=ax2.transAxes)
+    #  rots = [-30, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 30]
+    rots = [12, 9, 6, 3, 0]
+    #  colors = spec(np.linspace(0.14, 0.63, len(rots)))
+    colors = spec([0.14, 0.21, 0.35, 0.63, 0.70])
+    legs = [
+        Line2D(
+            [0], [0], linestyle="None", marker="$NS$", markerfacecolor="k", markersize=9
+        ),
+        Line2D(
+            [0],
+            [0],
+            linestyle="None",
+            marker="$EW$",
+            markerfacecolor="k",
+            markersize=9.6,
+        ),
+        Line2D([0], [0], color=colors[0], linewidth=2,),
+        Line2D(
+            [0],
+            [0],
+            color=colors[0],
+            linewidth=2,
+            linestyle="dotted",
+            dash_capstyle="round",
+        ),
+        Line2D([0], [0], color=colors[1], linewidth=2,),
+        Line2D(
+            [0],
+            [0],
+            color=colors[1],
+            linewidth=2,
+            linestyle="dotted",
+            dash_capstyle="round",
+        ),
+        Line2D([0], [0], color=colors[2], linewidth=2,),
+        Line2D(
+            [0],
+            [0],
+            color=colors[2],
+            linewidth=2,
+            linestyle="dotted",
+            dash_capstyle="round",
+        ),
+        Line2D([0], [0], color=colors[3], linewidth=2,),
+        Line2D(
+            [0],
+            [0],
+            color=colors[3],
+            linewidth=2,
+            linestyle="dotted",
+            dash_capstyle="round",
+        ),
+        Line2D([0], [0], color=colors[4], linewidth=2,),
+        Line2D(
+            [0],
+            [0],
+            color=colors[4],
+            linewidth=2,
+            linestyle="dotted",
+            dash_capstyle="round",
+        ),
+    ]
+    labels = [
+        None,
+        None,
+        r"$12\degree$",
+        r"$12\degree$",
+        r"$9\degree$",
+        r"$9\degree$",
+        r"$6\degree$",
+        r"$6\degree$",
+        r"$3\degree$",
+        r"$3\degree$",
+        r"$0\degree$",
+        r"$0\degree$",
+    ]
+
+    for i, r in enumerate(rots):
+
+        # rotate the ref feko model
+        ref_xx_rot = reproject_map(512, r, healpix_array=ref_xx)
+
+        res = ref_xx - ref_xx_rot
+
+        NS, EW = healpix_cardinal_slices(512, res, 85)
+
+        #  plt.plot(NS[1], NS[0], label="NS_XX")
+        #  plt.plot(EW[1], EW[0], label="EW_XX")
+        plt.plot(
+            NS[1][::36],
+            poly_fit(NS[1][::36], NS[0][::36], NS[0][::36], 9),
+            label=f"NS {r}",
+            color=colors[i],
+            linewidth=2,
+        )
+
+        plt.plot(
+            EW[1][::36],
+            poly_fit(EW[1][::36], EW[0][::36], EW[0][::36], 9),
+            label=f"EW {r}",
+            color=colors[i],
+            linewidth=2,
+            #  dashes=(0.5, 5.),
+            linestyle="dotted",
+            dash_capstyle="round",
+        )
+
     plt.xlabel("Zenith Angle [degrees]")
     plt.ylabel("Residual Power [dB]")
+
+    ax = fig.add_axes([0.27, 0.314, 0.57, 0.2])
+    ax.axis("off")
+    leg = ax.legend(legs, labels, mode="expand", ncol=6, frameon=True, loc="upper left")
+    leg.get_frame().set_facecolor("white")
+    for le in leg.legendHandles:
+        le.set_alpha(1)
+
     plt.tight_layout()
-    plt.savefig("ref_rot/rot_resi.pdf")
-
-
-slice_residual()
+    plt.savefig("ref_rot/rot_res_slices_2.pdf")
+    plt.close()
