713f3b46a6cc1dce67e98b1fc1a8a1a636f522b1
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-11-17 14:19:42 +1100

    Moving to v0.8.0 to fix cli-tools printing path to output file

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "embers"
 copyright = "2020, Aman Chokshi"
 author = "Aman Chokshi"
-version = "0.7.9"
+version = "0.8.0"
 
 # -- General configuration ---------------------------------------------------
 
