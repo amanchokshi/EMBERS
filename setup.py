bb55363bd27413617c075a7a8835c8a488ad10b9
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-11-20 00:48:29 +1100

    Moving to V0.8.3 to try and fix ephem_chrono

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def data_files(data_dir, data_types):
 
 setup(
     name="embers",
-    version="0.8.2",
+    version="0.8.3",
     license="MIT",
     author="Aman Chokshi",
     author_email="achokshi@student.unimelb.edu.au",
