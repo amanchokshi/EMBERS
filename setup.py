4ba203393e3013bee7a78db5051fd342b2621f9e
Aman Chokshi <aman.chokshi@mcgill.ca>
2026-04-01 16:50:26 -0400

    Drag embers kicking and screaming into modernity and py3.13

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def data_files(data_dir, data_types):
 
 setup(
     name="embers",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
     author="Aman Chokshi",
     author_email="achokshi@student.unimelb.edu.au",
